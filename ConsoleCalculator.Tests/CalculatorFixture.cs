803a08c2bca6a4b8d70a967da3c3aa6ed965c82d
tavisca-vraut <vraut@tavisca.in>
2019-06-30 00:35:45 +0530

    add implementation of calculator and unit tests code

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,65 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+        public Calculator calculator;
+        public CalculatorFixture() {
+            calculator = new Calculator();
+        }
+
         [Fact]
-        public void DummyTest()
-        {
-            return;
+        public void AddScenario() {
+            string keysPressed = "10+2=";
+
+            string result = FeedKeysAndGetResult(keysPressed);
+            Assert.Equal("12", result);
+        }
+
+        [Fact]
+        public void ErrorScenario() {
+            string keysPressed = "10/0=";
+
+            string result = FeedKeysAndGetResult(keysPressed);
+            Assert.Equal("-E-", result);
+        }
+
+        [Fact]
+        public void MultipleZeroesAndDecimal() {
+            string keysPressed = "00.001";
+
+            string result = FeedKeysAndGetResult(keysPressed);
+            Assert.Equal("0.001", result);
+        }
+
+        [Fact]
+        public void ChainingOperations() {
+            string keysPressed = "1+2+3+=";
+
+            string result = FeedKeysAndGetResult(keysPressed);
+            Assert.Equal("12", result);
+        }
+
+        [Fact]
+        public void SignToggling() {
+            string keysPressed = "12+2sss=";
+
+            string result = FeedKeysAndGetResult(keysPressed);
+            Assert.Equal("10", result);
+        }
+
+        [Fact]
+        public void CheckForClearScreen() {
+            string keysPressed = "1+2+C";
+
+            string result = FeedKeysAndGetResult(keysPressed);
+            Assert.Equal("0", result);
+        }
+
+        public string FeedKeysAndGetResult(string s) {
+            string result = "";
+            foreach(char key in s) {
+                result = calculator.SendKeyPress(key);
+            }
+            return result;
         }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -3,12 +3,139 @@
 namespace ConsoleCalculator
 {
     public class Calculator
-    {
+    {   
+        private string number1 = "";
+        private string number2 = "";
+        private char? previousOperator = null;
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            switch(key) {
+                case 'C':
+                case 'c':
+                    ClearMemory();
+                    return "0";
+                
+                case 'S':
+                case 's':
+                    // Only number 1 exists
+                    if (previousOperator == null) {
+                        StaticFunctions.ToggleSign(ref number1);
+                        return number1;
+                    }
+                    // Number1 in cache and number 2 under process
+                    StaticFunctions.ToggleSign(ref number2);
+                    return number2;
+                
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    // Only number 1 exists
+                    if (previousOperator == null) {
+                        // To avoid adding multiple 0s at the start
+                        if (number1 == "0") number1 = key.ToString();
+                        else number1 += key;
+                        return number1;
+                    }
+                    #region Number1 in cache and number 2 under process
+                        // To avoid adding multiple 0s at the start
+                        if (number2 == "0") number2 = key.ToString();
+                        else number2 += key;
+                        return number2;
+                    #endregion 
+                
+                case '.':
+                    // First condition: Only number 1 exists
+                    // Second condition: Avoid adding multiple floating points
+                    if (previousOperator == null && StaticFunctions.LastCharacterOf(number1) != '.') {
+                        number1 += key;
+                        return number1;
+                    }
+                    #region Number1 in cache and number 2 under process
+                        // Avoid adding multiple floating points
+                        if (StaticFunctions.LastCharacterOf(number2) != '.') number2 += key;
+                        return number2;
+                    #endregion 
+                
+                case '+':
+                case '-':
+                case 'X':
+                case 'x':
+                case '/':
+                    // Do the previous operation
+                    number1 = DoOperation();
+
+                    if (number1 == "error") return Error();
+
+                    number2 = "";
+
+                    // Assign current operation as previous Operation
+                    previousOperator = key;
+                    return number1;
+                
+                case '=':
+
+                    // Pressing '=' without any operations
+                    if (previousOperator == null) return number1;
+
+                    // Number 2 was not provided before hitting '=', so the displayed number is number2
+                    if (number2 == "") number2 = number1;
+
+                    number1 = SendKeyPress((char)previousOperator);
+                    
+                    if (number1 == "-E-") return Error();
+
+                    number2 = "";
+                    previousOperator = null;
+
+                    return number1;
+            }
+
+            return "";
+        }
+
+        public string DoOperation() {
+            
+            // If no pending previous operations
+            if (previousOperator == null) return this.number1;
+
+            float number1 = float.Parse(this.number1);
+            float number2 = float.Parse(this.number2);   
+
+            switch (previousOperator) {
+                case '+':
+                    return (number1 + number2).ToString();
+
+                case '-':
+                    return (number1 - number2).ToString();
+
+                case 'x':
+                case 'X':
+                    return (number1 * number2).ToString();
+                    
+                case '/':
+                    if (number2 == 0) return "error";
+                    return (number1 / number2).ToString();
+            }
+            return "";
+        }
+
+        public string Error() {
+            ClearMemory();
+            return "-E-";
+        }
+
+        public void ClearMemory() {
+            number1 = "";
+            number2 = "";
+            previousOperator = null;
         }
     }
 }

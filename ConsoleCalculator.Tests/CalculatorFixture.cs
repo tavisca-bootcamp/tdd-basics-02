66dda6fd92ba542e931ef6f2f4819470ec96466c
unknown <tjajodia@tavisca.in>
2019-06-29 17:26:23 +0530

    Assignment 2 solved

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,177 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+        Calculator obj;
+        public CalculatorFixture(){
+            this.obj = new Calculator();
+        }
         [Fact]
         public void DummyTest()
         {
             return;
+        }
+
+        [Fact]
+        public void ignorotherCharacters(){
+            string expected = "";
+            string actual = obj.SendKeyPress('g'); //tested with other invalid keys also
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void updatingScreenOnNumberPress(){
+
+            string expected = "57";
+            obj.SendKeyPress('5');
+            string actual = obj.SendKeyPress('7');
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void updatingScreenWhenOperatorIsPressed()
+        {
+            string expected = "75";
+            obj.SendKeyPress('7');
+            obj.SendKeyPress('5');
+
+            string actual = obj.SendKeyPress('+');
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void updatingScreenWhenNumberIsPressedAfterOperator()
+        {
+            
+            string expected = "2";
+            obj.SendKeyPress('4');
+            obj.SendKeyPress('3');
+            obj.SendKeyPress('+');
+            
+            string actual = obj.SendKeyPress('2');
+            
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void PerformingAddition()
+        {
+            string expected = "50";
+
+            obj.SendKeyPress('1');
+            obj.SendKeyPress('5');
+            obj.SendKeyPress('+');
+            obj.SendKeyPress('3');
+            obj.SendKeyPress('5');
+
+            string actual = obj.SendKeyPress('=');
+            
+            Assert.Equal(expected,actual);
+            
+        }
+
+        [Fact]
+        public void PerformingSubstraction()
+        {
+            string expected = "20";
+            obj.SendKeyPress('3');
+            obj.SendKeyPress('5');
+            obj.SendKeyPress('-');
+            obj.SendKeyPress('1');
+            obj.SendKeyPress('5');
+
+            string actual = obj.SendKeyPress('=');
+            
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void PerformingMultiplication()
+        {
+            string expected = "525";
+
+            obj.SendKeyPress('1');
+            obj.SendKeyPress('5');
+            obj.SendKeyPress('*');
+            obj.SendKeyPress('3');
+            obj.SendKeyPress('5');
+
+            string actual = obj.SendKeyPress('=');
+            
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void PerformingDivision()
+        {
+            string expected = "0.4285714";
+
+            obj.SendKeyPress('1');
+            obj.SendKeyPress('5');
+            obj.SendKeyPress('/');
+            obj.SendKeyPress('3');
+            obj.SendKeyPress('5');
+
+            string actual = obj.SendKeyPress('=');
+            
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void PressingResetKeyCheck()
+        {
+            string expected = "0";
+
+            obj.SendKeyPress('5');
+            obj.SendKeyPress('0');
+            obj.SendKeyPress('+');
+            obj.SendKeyPress('c');
+            obj.SendKeyPress('1');
+            obj.SendKeyPress('-');
+            obj.SendKeyPress('1');
+
+            string actual = obj.SendKeyPress('=');
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void PerformingFloatOperation()
+        {
+            string expected = "20";
+
+            obj.SendKeyPress('1');
+            obj.SendKeyPress('5');
+            obj.SendKeyPress('.');
+            obj.SendKeyPress('1');
+            obj.SendKeyPress('0');
+            obj.SendKeyPress('+');
+            obj.SendKeyPress('4');
+            obj.SendKeyPress('.');
+            obj.SendKeyPress('9');
+            obj.SendKeyPress('0');
+
+            string actual = obj.SendKeyPress('=');
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void PerformingNegation()
+        {
+            string expected = "20";
+
+            obj.SendKeyPress('3');
+            obj.SendKeyPress('5');
+            obj.SendKeyPress('+');
+            obj.SendKeyPress('1');
+            obj.SendKeyPress('5');
+            obj.SendKeyPress('s');
+
+            string actual = obj.SendKeyPress('=');
+
+            Assert.Equal(expected,actual);
         }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,142 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
+        private HashSet<char> possibleNumber;
+        private HashSet<char> possibleOperator;
+        private HashSet<char> possibleSpecialChar;
+        private float firstNumber;
+        private float secodNumber;
+        private char inputOperator;
+        private float result;
+        private bool isDecimalEntry;
 
+        public Calculator()
+        {
+            possibleNumber = new HashSet<char>{'0','1','2','3','4','5','6','7','8','9'};
+            possibleOperator = new HashSet<char>{'-','+','*','/','='};
+            possibleSpecialChar = new HashSet<char>{'s','c'};
+            firstNumber = 0;
+            secodNumber = 0;
+            inputOperator = 'n';
+            result = 0;
+            isDecimalEntry = true;
+        }
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+           if(possibleNumber.Contains(key)){
+               if(isDecimalEntry){
+                    if(inputOperator.Equals('n'))
+                    {
+                        firstNumber = firstNumber * 10;
+                        firstNumber += int.Parse(key.ToString());
+                        return firstNumber.ToString();
+                    }
+                    else{
+                        secodNumber = secodNumber * 10;
+                        secodNumber += int.Parse(key.ToString());
+                        return secodNumber.ToString();
+                    }
+               }
+               else{
+                    if(inputOperator.Equals('n'))
+                    {
+                        float number = int.Parse(key.ToString());
+                        number /= 10;
+                        firstNumber += number;
+                        return firstNumber.ToString();
+                    }
+                    else{
+                        float number = int.Parse(key.ToString());
+                        number /= 10;
+                        secodNumber += number;
+                        return secodNumber.ToString();
+                    }
+               }
+           }
+           else if(key.Equals('.')){
+                   isDecimalEntry = false;
+                   if(inputOperator.Equals('n'))
+                        return firstNumber.ToString() + ".";
+                    else
+                        return secodNumber.ToString() + ".";
+           }
+           else if(possibleOperator.Contains(key)){
+                string returningNumber = "";
+
+                if(key.Equals('='))
+                    return calculate();
+
+                //if this condition evalutes to true it means that the operator has been pressed just after the firstNumber and the display should display the firstNumber
+                if(result == 0 && secodNumber == 0)
+                    returningNumber =  firstNumber.ToString();
+                else // the secondNumber has also been inputted and the operator has been inputted in the previos call and now the result needs to be calculated 
+                    returningNumber =  calculate();
+                
+                inputOperator = key;
+                isDecimalEntry = true;
+                return returningNumber;
+
+           }
+           else if(key.Equals('c')){
+               firstNumber = 0;
+               secodNumber = 0;
+               inputOperator = 'n';
+               result = 0;
+
+               return "0";
+           }
+           else if(key.Equals('s')){
+               if(inputOperator.Equals('n')){
+                   firstNumber = -firstNumber;
+                   return firstNumber.ToString();
+               }
+               else{
+                   secodNumber = -secodNumber;
+                   return secodNumber.ToString();
+               }
+           }
+           
+
+           return "";
         }
-    }
+
+        private string calculate()
+        {
+            
+            switch(inputOperator){
+                case '+':
+                {
+                    result = firstNumber + secodNumber;
+                    break;
+                }
+                case '-':
+                {
+                    result = firstNumber - secodNumber;
+                    break;
+                }
+                case '*':
+                {
+                    result = firstNumber * secodNumber;
+                    break;
+                }
+                case '/':
+                {
+                    result = firstNumber / secodNumber;
+                    break;
+                }
+                case 'n':{
+                    result = firstNumber;
+                    break;
+                }
+            }
+            
+            firstNumber = result;
+            secodNumber = 0;
+            result = 0;
+            return firstNumber.ToString();
+    }   }
 }

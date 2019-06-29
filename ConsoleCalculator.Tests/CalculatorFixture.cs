89a5954129f66c3bdbc7e57c0d9ab1c413729d52
tavisca-cpadole <51901832+tavisca-cpadole@users.noreply.github.com>
2019-06-29 22:39:56 +0530

    Added TestCases

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,73 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+        Calculator calci = new Calculator();
+        string query = "";
+        string actualOutput = "";
         [Fact]
-        public void DummyTest()
+        public void DivisibilityByZero()
         {
-            return;
+            query = "1/0=";
+            actualOutput = FetchActualOutput(query);
+            Assert.Equal("-E-", actualOutput);
+        }
+
+        [Fact]
+        public void TestForClearScreen()
+        {
+            query = "1/99=c";
+            actualOutput = FetchActualOutput(query);
+            Assert.Equal("0", actualOutput);
+        }
+
+        [Fact]
+        public void IsValidOutput()
+        {
+            query = "18x2-20+1/1=";
+            actualOutput = FetchActualOutput(query);
+            Assert.Equal("17", actualOutput);
+        }
+
+        [Fact]
+        public void CanHandleCaseSensitivityForSign()
+        {
+            query = "1+2S=";
+            actualOutput = FetchActualOutput(query);
+            Assert.Equal("-1", actualOutput);
+        }
+
+        [Fact]
+        public void CanHandleCaseSensitivityForClear()
+        {
+            query = "1+2sS=c1+2C";
+            actualOutput = FetchActualOutput(query);
+            Assert.Equal("0", actualOutput);
+        }
+
+        [Fact]
+        public void CanHandleCaseSensitivityForMuliplication()
+        {
+            query = "2x2X2=";
+            actualOutput = FetchActualOutput(query);
+            Assert.Equal("8", actualOutput);
+        }
+
+        [Fact]
+        public void CanHandlePositiveNegativeNumbers()
+        {
+            query = "1+-1=";
+            actualOutput = FetchActualOutput(query);
+            Assert.Equal("0", actualOutput);
+        }
+
+
+        public string FetchActualOutput(string query) {
+            string temp = "";
+            foreach (char input in query)
+            {
+                temp = calci.SendKeyPress(input);
+            }
+            return temp;
         }
     }
 }

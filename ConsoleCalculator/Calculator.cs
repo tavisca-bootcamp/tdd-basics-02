f20176a94c7e94e93abc194e1d76ebbbc330bb49
tavisca-dshrivastav <dshrivastav@tavisca.in>
2019-06-29 19:56:06 +0530

    Implemented Calculator Code using xunit (By TDD)

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -4,11 +4,88 @@ namespace ConsoleCalculator
 {
     public class Calculator
     {
+        CalculatorUtility calculatorUtility = new CalculatorUtility();
 
+        public double _firstNumber = 0;
+        public double _secondNumber = 0;
+        public string _integer = "";
+        public char _operator;
+        public string _result = "";
+        public int _operatorCount = 0;
+        public bool _isDecimalPointUsed = false;
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            if (calculatorUtility.IsResetKey(key))
+            {
+                _integer = "";
+                _firstNumber = 0;
+                _secondNumber = 0;
+                _operatorCount = 0;
+                _isDecimalPointUsed = false;
+                _result = "";
+            }
+            else if (calculatorUtility.IsNumber(key) || calculatorUtility.IsDecimalPoint(key))
+            {
+                
+                if (_isDecimalPointUsed)
+                {
+                    if (!calculatorUtility.IsDecimalPoint(key))
+                        _integer += key;
+                }
+                else
+                    _integer += key;
+                if (calculatorUtility.IsDecimalPoint(key))
+                    _isDecimalPointUsed = true;
+                _result = _integer;
+            }
+            else if (calculatorUtility.IsNegativeInteger(key))
+            {
+                _integer = (-double.Parse(_integer)).ToString();
+                _result = _integer;
+            }
+            else if (calculatorUtility.IsArithmeticOperator(key))
+            {
+                if (_operatorCount >= 1)
+                {
+                    _operatorCount++;
+                    _secondNumber = double.Parse(_integer);
+                    if (calculatorUtility.IsAdd(_operator))
+                        _result = calculatorUtility.Add(_firstNumber, _secondNumber);
+                    else if (calculatorUtility.IsSubstract(_operator))
+                        _result = calculatorUtility.Substract(_firstNumber, _secondNumber);
+                    else if (calculatorUtility.IsMultiply(_operator))
+                        _result = calculatorUtility.Multiply(_firstNumber, _secondNumber);
+                    else if (calculatorUtility.IsDivide(_operator))
+                    { 
+                        if (_secondNumber == 0)
+                            return "-E-";
+                        _result = calculatorUtility.Divide(_firstNumber, _secondNumber);
+                    }
+                    _isDecimalPointUsed = false;
+                    _integer = "";
+                    _firstNumber = double.Parse(_result);
+                }
+                else if (!calculatorUtility.IsEqualOperator(key))
+                {
+                    //_integer = "";
+                    _operator = key;
+                }
+                if (!calculatorUtility.IsEqualOperator(key) && _operatorCount < 1)
+                {
+                    _operatorCount++;
+                    _firstNumber = double.Parse(_integer);
+                    _result = _integer;
+                    _isDecimalPointUsed = false;
+                    _integer = "";
+
+                    return _result;
+                }
+                
+            }
+            return _result;
+            //throw new NotImplementedException();
         }
+
+
     }
 }
---
+++ ConsoleCalculator/CalculatorUtility.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    public class CalculatorUtility
+    {
+
+        //public double FirstNumber { get; set; }
+        //public double SecondNumber { get; set; }
+        //public string Number { get; set; }
+        //public char OperatorKey { get; set; }
+        //public int OperatorOccurance { get; set; }
+        //public bool Is { get; set; }
+        public bool IsResetKey(char key)
+        {
+            if (key == 'c' || key == 'C')
+                return true;
+            else
+                return false;
+            throw new NotImplementedException();
+        }
+
+        public string Divide(double firstNumber, double secondNumber)
+        {
+            return (firstNumber / secondNumber).ToString();
+            throw new NotImplementedException();
+        }
+
+        public string Multiply(double firstNumber, double secondNumber)
+        {
+            return (firstNumber * secondNumber).ToString();
+            throw new NotImplementedException();
+        }
+
+        public string Substract(double firstNumber, double secondNumber)
+        {
+            return (firstNumber - secondNumber).ToString();
+            throw new NotImplementedException();
+        }
+
+        public string Add(double firstNumber, double secondNumber)
+        {
+            return (firstNumber + secondNumber).ToString();
+            throw new NotImplementedException();
+        }
+
+        public bool IsDivide(char key)
+        {
+            if (key == '/')
+                return true;
+            else return false;
+            throw new NotImplementedException();
+        }
+
+        public bool IsMultiply(char key)
+        {
+            if (key == 'x' || key == 'X')
+                return true;
+            else
+                return false;
+            throw new NotImplementedException();
+        }
+
+        public bool IsSubstract(char key)
+        {
+            if (key == '-')
+                return true;
+            else
+                return false;
+            throw new NotImplementedException();
+        }
+
+        public bool IsAdd(char key)
+        {
+            if (key == '+')
+                return true;
+            else
+                return false;
+            throw new NotImplementedException();
+        }
+
+        public bool IsDecimalPoint(char key)
+        {
+            if (key == '.')
+                return true;
+            else
+                return false;
+            throw new NotImplementedException();
+        }
+
+        public bool IsNegativeInteger(char key)
+        {
+            if (key == 's' || key == 'S')
+                return true;
+            else
+                return false;
+            throw new NotImplementedException();
+        }
+
+        public bool IsEqualOperator(char key)
+        {
+            if (key == '=')
+                return true;
+            else
+                return false;
+            throw new NotImplementedException();
+        }
+
+        public bool IsArithmeticOperator(char key)
+        {
+            if (key == '+' || key == '-' || key == 'x' || key == 'X' || key == '/' || key == '=')
+                return true;
+            else
+                return false;
+            throw new NotImplementedException();
+        }
+
+        public bool IsNumber(char key)
+        {
+            if (key >= '0' && key <= '9')
+                return true;
+            else
+                return false;
+            //throw new NotImplementedException();
+        }
+    }
+}

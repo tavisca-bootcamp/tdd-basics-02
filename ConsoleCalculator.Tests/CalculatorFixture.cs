d4616ed99cd6ddbd8f1eca9da9195188ce5bf69b
Ujjwal Bharti <ubharti@tavisca.in>
2019-06-30 11:32:44 +0530

    Add implementation of ConsoleCalculator App with Unit test

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -1,14 +1,168 @@
-using System;
 using Xunit;
 
 namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
-        [Fact]
-        public void DummyTest()
+        Calculator calculator = new Calculator();
+
+        private string sendMultipleKey(char[] key)
         {
-            return;
+            var i = 0;
+            for (i = 0; i < key.Length - 1; i++)
+                calculator.SendKeyPress(key[i]);
+            return calculator.SendKeyPress(key[i]);
+        }
+
+
+        [Fact]
+        public void TestKeyPress()
+        {
+            char key = '1';
+
+            string fromCall = calculator.SendKeyPress(key);
+
+            Assert.Equal("1", fromCall);
+        }
+
+        [Fact]
+        public void TestMultipleKeyPress()
+        {
+            char[] key = new char[] { 'h', '1', '2' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("12", fromCall);
+        }
+
+        [Fact]
+        public void TestZeroKeySequence()
+        {
+            char[] key = new char[] { 'h', '0', '0','0' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("0", fromCall);
+        }
+
+        [Fact]
+        public void TestDecimalKeySequence()
+        {
+            char[] key = new char[] { 'h', '0', '.', '.' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("0.", fromCall);
+        }
+
+        [Fact]
+        public void TestBlankScreenDecimalKeySequence()
+        {
+            char[] key = new char[] { 'h', '.', '.' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("0.", fromCall);
+        }
+
+
+        [Fact]
+        public void TestOperatorKeyPress()
+        {
+            char[] key = new char[] { 'h', '1', '2','+' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("12", fromCall);
+        }
+
+        [Fact]
+        public void TestOperatorAndKeyPress()
+        {
+            char[] key = new char[] { 'h', '1', '2', '+','1' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("1", fromCall);
+        }
+        [Fact]
+        public void TestSignChange()
+        {
+            char[] key = new char[] { 'h', '1', 's','s' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("1", fromCall);
+        }
+
+        [Fact]
+        public void TestAddition()
+        {
+            char[] key = new char[] { 'h', '1', '2', '+', '1' ,'='};
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("13", fromCall);
+        }
+
+        [Fact]
+        public void TestDivision()
+        {
+            char[] key = new char[] { 'h', '1', '/', '0','='};
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("-E-", fromCall);
+        }
+
+        [Fact]
+        public void TestSignAirthmetic()
+        {
+            char[] key = new char[] { 'h', '1', '+', '3','s','S' ,'=' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("4", fromCall);
+        }
+
+        [Fact]
+        public void TestArithmeticCalculationWithC()
+        {
+            char[] key = new char[] { '1', '+', '2', '+', 'C' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("0", fromCall);
+        }
+
+        [Fact]
+        public void TestAnotherCalculation()
+        {
+            char[] key = new char[] { '1', '+', '2', '+', '3','+','=' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("12", fromCall);
+        }
+
+        [Fact]
+        public void TestSomeCalulation()
+        {
+            char[] key = new char[] { '1','2' ,'+', '2', 'S', 's', 'S', '=' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("10", fromCall);
+        }
+
+        [Fact]
+        public void TestZeroAndDecimal()
+        {
+            char[] key = new char[] { '0', '0', '.', '.', '0', '0', '1' };
+
+            string fromCall = sendMultipleKey(key);
+
+            Assert.Equal("0.001", fromCall);
         }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,194 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
+        public StringBuilder firstNumber = new StringBuilder("");
+        public StringBuilder secondNumber = new StringBuilder("");
+        public StringBuilder display = new StringBuilder("");
+        public int mode = 0;
+        public char _operator = 'z';
+        public List<char> symbolList = new List<char>() { '-', '+', '/', '=', 'x', 'X', 'c', 'C', 's', 'S' };
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            if(IsValidKey(key))
+            {
+                if (IsOperator(key))
+                {
+                    PerformOperation(key);
+                    return display.ToString();
+                }
+                else if(key=='.')
+                {
+                    return HandleDecimal('.');
+                }
+                else if(key=='0')
+                {
+                    return HandleZero('0');
+                }
+                else
+                {
+                    PerformNumberDisplay(key);
+                    return display.ToString();
+                }
+            }
+            else
+            {
+                return display.ToString();
+            }
+        }
+
+        private string HandleZero(char key)
+        {
+            if (display.Length == 1)
+            {
+                if (display[0] == '0')
+                    return display.ToString();
+            }
+            PerformNumberDisplay(key);
+            return display.ToString();
+        }
+
+        private string  HandleDecimal(char key)
+        {
+            if (display.Length == 0)
+                display.Append("0.");
+            else if (display[display.Length - 1] != '.')
+                PerformNumberDisplay(key);
+            return display.ToString();
+        }
+
+        private void PerformNumberDisplay(char key)
+        {
+            if(mode==1)
+            {
+                display.Clear();
+                mode = 0;
+            }
+            display.Append(key);
+        }
+
+        private void PerformOperation(char key)
+        {
+            var arithmeticList = new List<char>() { '+', 'x', 'X', '-', '/' };
+            if(arithmeticList.Contains(key))
+            {
+                PerformOperation('=');
+                firstNumber.Clear();
+                firstNumber.Append(display.ToString());
+                mode = 1;
+                _operator = key;
+            }
+            else
+            {
+                switch(key)
+                {
+                    case '=':
+                        if(_operator!='z')
+                        {
+                            secondNumber.Clear();
+                            secondNumber.Append(display.ToString());
+                            PerformCalculation();
+                            _operator = 'z';
+                            mode = 1;
+                        }
+                        break;
+                    case 's':
+                    case 'S':
+                        decimal result = decimal.Multiply(-1, decimal.Parse(display.ToString()));
+                        display.Clear();
+                        display.Append(result.ToString());
+                        break;
+                    case 'c':
+                    case 'C':
+                        _operator = 'z';
+                        firstNumber.Clear();
+                        secondNumber.Clear();
+                        display.Clear();
+                        display.Append('0');
+                        break;
+                }
+            }
+        }
+
+        private void PerformCalculation()
+        {
+            decimal result;
+            bool firstCondition = decimal.TryParse(firstNumber.ToString(), out decimal fNumber);
+            bool secondContition =decimal.TryParse(secondNumber.ToString(), out decimal SNumber);
+            if (firstCondition && secondContition)
+            {
+                result = GetResult(fNumber, SNumber);
+                if(result!=decimal.MinValue)
+                {
+                    display.Clear();
+                    if (decimal.Ceiling(result) - decimal.Floor(result) == 0)
+                    {
+                        display.Append(decimal.ToInt32(result).ToString());
+                    }
+                    else
+                        display.Append(result.ToString());
+                }
+            }
+            else
+            {
+                display.Clear();
+                display.Append("-E-");
+            }
+
+        }
+
+        private decimal GetResult(decimal fNumber, decimal sNumber)
+        {
+            decimal result=decimal.MinValue;
+            switch(_operator)
+            {
+                case '+':
+                    result = fNumber + sNumber;
+                    break;
+                case '-':
+                    result = fNumber - sNumber;
+                    break;
+                case 'x':
+                case 'X':
+                    result = fNumber * sNumber;
+                    break;
+                case '/':
+                    try
+                    {
+                        result = fNumber / sNumber;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        display.Clear();
+                        display.Append("-E-");
+                    }
+                    break;
+            }
+            return result;
+        }
+
+        private bool IsValidKey(char key)
+        {
+            if (char.IsDigit(key))
+                return true;
+            else if (IsOperator(key)||key=='.')
+                return true;
+            else
+                return false;
+        }
+
+        private bool IsOperator(char key)
+        {
+            
+            if (symbolList.Contains(key))
+                return true;
+            else
+                return false;
         }
     }
 }

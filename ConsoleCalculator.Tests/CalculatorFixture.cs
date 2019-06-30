e3a54d6a3c94fc787c1d8ba2595ec2e9f5b38215
visharma <visharma@tavisca.in>
2019-06-30 08:42:30 +0530

    Solved Assignment 2 of Unit Testing Successfully

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -6,9 +6,156 @@ namespace ConsoleCalculator.Tests
     public class CalculatorFixture
     {
         [Fact]
-        public void DummyTest()
+        public void SuccessfullArithmeticOperationTest()
         {
-            return;
+            string equation;
+            string actual;
+            string expected;
+
+            equation = "10+2=";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "12";
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void ErrorConditionTest()
+        {
+            string equation;
+            string actual;
+            string expected;
+
+            equation = "10/0=";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "-E-";
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void ZerosConditionsTest()
+        {
+            string equation;
+            string actual;
+            string expected;
+
+            equation = "00..001";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "0.001";
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void ToggleConditionTest()
+        {
+            string equation;
+            string actual;
+            string expected;
+
+            equation = "12+2sSs=";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "10";
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void PropertyOfEqualSignTest()
+        {
+            string equation;
+            string actual;
+            string expected;
+
+            equation = "1+2+3+=";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "12";
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void ClearConditionTest()
+        {
+            string equation;
+            string actual;
+            string expected;
+
+            equation = "1+2+C";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "0";
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void MultiplicationSignTest()
+        {
+            string equation;
+            string actual;
+            string expected;
+
+            equation = "5x2X7=";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "70";
+
+            Assert.Equal(expected,actual);
+        }
+        
+        [Fact]
+        public void IgnoreInputsOtherThanValidDigitsAndOperators()
+        {
+            string equation;
+            string actual;
+            string expected;
+
+            equation = "1aidb5+2tbjd#$@0 - 5=";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "30";
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void CheckDecimalPointWorkingTest()
+        {
+            string equation;
+            string actual;
+            string expected;
+
+            equation = "7..1 + 10...2 = ";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "17.3";
+
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void DefaultValueOfCalculatorTest()
+        {
+            string equation;
+            string actual;
+            string expected;
+
+            equation = " ";
+            actual = utilityFuntionToProcessOperandsEquation(equation);
+            expected = "0";
+
+            Assert.Equal(expected,actual);
+        }
+
+        /*
+            Utility funtion to process operands equation in a string format.
+        */
+
+        public string utilityFuntionToProcessOperandsEquation(string equation){
+            Calculator calc = new Calculator();
+            string output = "";
+            foreach(char ch in equation){
+                output = calc.SendKeyPress(ch);
+            }
+            return output;
         }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -2,13 +2,177 @@
 
 namespace ConsoleCalculator
 {
+    class InvalidInputDigitException : Exception
+    {
+        public InvalidInputDigitException(string message) : base(message)
+        {
+        }
+    }
     public class Calculator
     {
 
+        string operand1 = "0";
+        string operand2 = "";
+        char previousOperator = '+';
+        string displayOutput = "0";
+        bool isDecimalEncountered = false;
+        string previousOperandResult = "";
+
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            string output = "0";
+            try
+            {
+                output = validateAndPerformOperationsForKey(key);
+            }
+            catch (InvalidInputDigitException)
+            {
+                output = displayOutput;
+            }
+            catch (DivideByZeroException)
+            {
+                output = "-E-";
+            }
+            return output;
+        }
+
+        private string validateAndPerformOperationsForKey(char key)
+        {
+            if (isClearKey(key))
+            {
+                resetCalculatorAtInitialStage();
+                displayOutput = "0";
+            }
+            else if (isToggleKey(key))
+            {
+                toggleSignOfOperand();
+                displayOutput = operand2;
+            }
+            else if (isDecimalOperator(key))
+            {
+                if (!isDecimalEncountered)
+                {
+                    operand2 = operand2 + key;
+                    isDecimalEncountered = true;
+                    displayOutput = operand2;
+                }
+            }
+            else if (isValidOperator(key))
+            {
+                isDecimalEncountered = false;
+                if (key == '=' && operand2 == "")
+                    operand2 = previousOperandResult;
+                performPreviousOperation();
+                displayOutput = operand1;
+                // operand1 = operand2;
+                
+                if (key != '=')
+                {
+                    previousOperator = key;
+                    previousOperandResult = operand1;
+                }else{
+                    previousOperandResult = operand2;
+                }
+                operand2 = "";
+            }
+            else if (isValidDigit(key))
+            {
+                if (key == '0')
+                    validateZerosConditions(key);
+                else
+                    operand2 = operand2 + key;
+                displayOutput = operand2;
+            }
+            else
+            {
+                throw new InvalidInputDigitException($"Invalid digit '{key}' is passed in the Input Field");
+            }
+            return displayOutput;
+        }
+
+        private void validateZerosConditions(char key)
+        {
+            if (isDecimalEncountered)
+            {
+                operand2 = operand2 + key;
+            }
+            else
+            {
+                operand2 = Int32.Parse(operand2 + key).ToString();
+            }
+        }
+
+        private bool isDecimalOperator(char key)
+        {
+            return key == '.';
+        }
+
+        private void toggleSignOfOperand()
+        {
+            operand2 = (-1 * Double.Parse(operand2)).ToString();
+        }
+
+        private bool isToggleKey(char key)
+        {
+            return key == 'S' || key == 's';
+        }
+
+        private bool isClearKey(char key)
+        {
+            return key == 'C' || key == 'c';
+        }
+
+        private void resetCalculatorAtInitialStage()
+        {
+            operand1 = "0";
+            operand2 = "";
+            previousOperator = '+';
+            displayOutput = "0";
+            isDecimalEncountered = false;
+            previousOperandResult = "";
+        }
+
+        private void performPreviousOperation()
+        {
+            if (operand2 == "")
+                return;
+            switch (previousOperator)
+            {
+                case '+':
+                    operand1 = (Double.Parse(operand1) + Double.Parse(operand2)).ToString();
+                    break;
+                case '-':
+                    operand1 = (Double.Parse(operand1) - Double.Parse(operand2)).ToString();
+                    break;
+                case 'X':
+                case 'x':
+                    operand1 = (Double.Parse(operand1) * Double.Parse(operand2)).ToString();
+                    break;
+                case '/':
+                    if (operand2 == "0")
+                        throw new DivideByZeroException("Can't Divide By Zero");
+                    operand1 = (Double.Parse(operand1) / Double.Parse(operand2)).ToString();
+                    break;
+                // case '=':
+                //     operand2 = operand1;
+                //     break;
+                default:
+                    Console.WriteLine("Invalid Operator");
+                    break;
+            }
+        }
+        private bool isValidDigit(char key)
+        {
+            if (key >= '0' && key <= '9')
+                return true;
+            return false;
+        }
+
+        private bool isValidOperator(char key)
+        {
+            if (key == '+' || key == '-' || key == 'x' || key == 'X' || key == '/' || key == '=')
+                return true;
+            return false;
         }
     }
 }

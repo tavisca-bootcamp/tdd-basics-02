542d0e5d8c7874aaf3f8208779f0045aed886866
Md Ebran Khan <ekhan@tavisca.in>
2019-06-30 20:29:20 +0530

    Solved Assignment 3.2

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,88 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+        Calculator calculator = new Calculator();
+
         [Fact]
-        public void DummyTest()
+        public void AddTwoNumbers()
         {
-            return;
+            string result = GetResultForGivenExpression("10+2=");
+            Assert.Equal("12", result);
+        }
+
+        [Fact]
+        public void SubtractTwoNumbers()
+        {
+            string result = GetResultForGivenExpression("10-2=");
+            Assert.Equal("8", result);
+        }
+
+        [Fact]
+        public void MultiplyTwoNumbersUsingUpperCaseX()
+        {
+            string result = GetResultForGivenExpression("10X2=");
+            Assert.Equal("20", result);
+        }
+
+        [Fact]
+        public void MultiplyTwoNumbersUsingLowerCaseX()
+        {
+            string result = GetResultForGivenExpression("10x2=");
+            Assert.Equal("20", result);
+        }
+
+        [Fact]
+        public void DivideTwoNumbers()
+        {
+            string result = GetResultForGivenExpression("10/2=");
+            Assert.Equal("5", result);
+        }
+
+        [Fact]
+        public void DivideByZero()
+        {
+            string result = GetResultForGivenExpression("10/0=");
+            Assert.Equal("-E-", result);
+        }
+
+        [Fact]
+        public void FloatingPointDivision()
+        {
+            string result = GetResultForGivenExpression("10/3=");
+            Assert.Equal("3.333333", result);
+        }
+
+        [Fact]
+        public void SignChange()
+        {
+            string result = GetResultForGivenExpression("10s");
+            Assert.Equal("-10", result);
+        }
+
+        [Fact]
+        public void ClearScreen()
+        {
+            string result = GetResultForGivenExpression("1+2+C");
+            Assert.Equal("0", result);
+        }
+
+        [Fact]
+        public void MultipleZerosBeforeDecimalPoint()
+        {
+            string result = GetResultForGivenExpression("00.001");
+            Assert.Equal("0.001", result);
+        }
+
+
+
+        private string GetResultForGivenExpression(string expression)
+        {
+            string result = "";
+            foreach (char key in expression)
+            {
+                result = calculator.SendKeyPress(key);
+            }
+            return result;
         }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -4,11 +4,135 @@ namespace ConsoleCalculator
 {
     public class Calculator
     {
+        private string firstNumber = "";
+        private string secondNumber = "";
+        private char? lastOperator = null;
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            switch (char.ToLower(key))
+            {
+                case 'c':
+                    ResetCalculator();
+                    return "0";
+
+                case 's':
+                    if (lastOperator == null)
+                    {
+                        ChangeSign(ref firstNumber);
+                        return firstNumber;
+                    }
+                    ChangeSign(ref secondNumber);
+                    return secondNumber;
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    if (lastOperator == null)
+                    {
+                        if (firstNumber == "0")
+                            firstNumber = key.ToString();
+                        else
+                            firstNumber += key;
+                        return firstNumber;
+                    } 
+                    if (secondNumber == "0")
+                        secondNumber = key.ToString();
+                    else
+                        secondNumber += key;
+                    return secondNumber;
+
+                case '.':
+                    if (lastOperator == null && firstNumber[firstNumber.Length - 1] != '.')
+                    {
+                        firstNumber += key;
+                        return firstNumber;
+                    }
+                    if (secondNumber[secondNumber.Length - 1] != '.')
+                        secondNumber += key;
+                    return secondNumber;
+
+                case '+':
+                case '-':
+                case 'x':
+                case '/':
+                    firstNumber = Calculate();
+                    if (firstNumber == "error")
+                        return ThrowError();
+                    secondNumber = "";
+                    lastOperator = key;
+
+                    return firstNumber;
+
+                case '=':
+                    if (lastOperator == null)
+                        return firstNumber;
+                    if (secondNumber == "")
+                        secondNumber = firstNumber;
+
+                    firstNumber = SendKeyPress((char)lastOperator);
+
+                    if (firstNumber == "-E-")
+                        return ThrowError();
+                    secondNumber = "";
+                    lastOperator = null;
+
+                    return firstNumber;
+            }
+            return "";
+        }
+
+        private string Calculate()
+        {
+            if (lastOperator == null)
+                return this.firstNumber;
+
+            float.TryParse(this.firstNumber, out float firstNumber);
+            float.TryParse(this.secondNumber, out float secondNumber);
+
+            switch(char.ToLower((char)lastOperator))
+            {
+                case '+':
+                    return (firstNumber + secondNumber).ToString();
+
+                case '-':
+                    return (firstNumber - secondNumber).ToString();
+
+                case 'x':
+                    return (firstNumber * secondNumber).ToString();
+
+                case '/':
+                    if (secondNumber == 0)
+                        return "error";
+                    return (firstNumber / secondNumber).ToString();
+            }
+
+            return "";
+        }
+        private string ThrowError()
+        {
+            ResetCalculator();
+            return "-E-";
+        }
+
+        private static void ChangeSign(ref string number)
+        {
+            if (float.TryParse(number, out float n))
+                number = (-1 * n).ToString();
+        }
+
+        private void ResetCalculator()
+        {
+            firstNumber = "";
+            secondNumber = "";
+            lastOperator = null;
         }
     }
 }

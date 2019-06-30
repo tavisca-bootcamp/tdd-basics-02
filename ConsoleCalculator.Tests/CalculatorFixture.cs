7803e3b3338f7d627b6aa064f032e8a9772b257e
Rishabh <rjaiswal@tavisca.in>
2019-06-30 16:12:05 +0530

    Solved Assignment2

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,73 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+        private Calculator c = new Calculator();
+
         [Fact]
-        public void DummyTest()
+        public void Test1()
         {
-            return;
+            Assert.Equal("1", c.SendKeyPress('1'));
+            Assert.Equal("10", c.SendKeyPress('0'));
+            Assert.Equal("10", c.SendKeyPress('+'));
+            Assert.Equal("2", c.SendKeyPress('2'));
+            Assert.Equal("12", c.SendKeyPress('='));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal("1", c.SendKeyPress('1'));
+            Assert.Equal("10", c.SendKeyPress('0'));
+            Assert.Equal("10", c.SendKeyPress('/'));
+            Assert.Equal("0", c.SendKeyPress('0'));
+            Assert.Equal("-E-", c.SendKeyPress('='));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal("0", c.SendKeyPress('0'));
+            Assert.Equal("0", c.SendKeyPress('0'));
+            Assert.Equal("0.", c.SendKeyPress('.'));
+            Assert.Equal("0.", c.SendKeyPress('.'));
+            Assert.Equal("0.0", c.SendKeyPress('0'));
+            Assert.Equal("0.00", c.SendKeyPress('0'));
+            Assert.Equal("0.001", c.SendKeyPress('1'));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Assert.Equal("1", c.SendKeyPress('1'));
+            Assert.Equal("12", c.SendKeyPress('2'));
+            Assert.Equal("12", c.SendKeyPress('+'));
+            Assert.Equal("2", c.SendKeyPress('2'));
+            Assert.Equal("-2", c.SendKeyPress('S'));
+            Assert.Equal("2", c.SendKeyPress('S'));
+            Assert.Equal("-2", c.SendKeyPress('S'));
+            Assert.Equal("10", c.SendKeyPress('='));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Assert.Equal("1", c.SendKeyPress('1'));
+            Assert.Equal("1", c.SendKeyPress('+'));
+            Assert.Equal("2", c.SendKeyPress('2'));
+            Assert.Equal("3", c.SendKeyPress('+'));
+            Assert.Equal("3", c.SendKeyPress('3'));
+            Assert.Equal("6", c.SendKeyPress('+'));
+            Assert.Equal("12", c.SendKeyPress('='));
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            Assert.Equal("1", c.SendKeyPress('1'));
+            Assert.Equal("1", c.SendKeyPress('+'));
+            Assert.Equal("2", c.SendKeyPress('2'));
+            Assert.Equal("3", c.SendKeyPress('+'));
+            Assert.Equal("0", c.SendKeyPress('c'));
         }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,93 @@
 ﻿using System;
+using System.Data;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
+        private string expression = "";
+        private string number = "";
+        private string consoleDisplay = "";
+        private DataTable dt = new DataTable();
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            return display(key);
+        }
+
+        private string display(char key)
+        {
+            if(key=='c')
+            {
+                consoleDisplay = "0";
+                expression = number = "";
+            }
+            else if(isOperator(key))
+            {
+                expression += consoleDisplay;
+                number = "";
+                expression = consoleDisplay = evaluateExpression(expression);
+                expression += key;
+            }
+            else if(isNumber(key))
+            {
+                number = evaluateExpression(number += key);
+                consoleDisplay = number;
+            }
+            else if(key=='.' && !consoleDisplay.Contains("."))
+            {
+                number += key;
+                consoleDisplay = number;
+            }
+            else if(key=='S'||key=='s')
+            {
+                number= -float.Parse(number)+"";
+                consoleDisplay = number;
+            }
+            else if (key=='=')
+            {
+                if (number=="")
+                {
+                    string c = expression.Substring(0, expression.Length - 1);
+                    expression = expression + c;
+                }
+                else
+                {
+                    expression += number;
+                }
+                consoleDisplay = evaluateExpression(expression);
+            }
+            if(consoleDisplay== "∞")
+            {
+                consoleDisplay = "-E-";
+            }
+            return consoleDisplay;
+        }
+
+        private string evaluateExpression(string expression)
+        {
+            expression = expression.Replace('x', '*');
+            expression = expression.Replace('X', '*');
+            var exp = dt.Compute(expression, "");
+            return exp+"";
+        }
+
+        private bool isNumber(char key)
+        {
+            if(Char.IsDigit(key))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isOperator(char key)
+        {
+            if(key=='+'|| key == '-' || key == 'x' || key == 'X' || key == '/')
+            {
+                return true;
+            }
+            return false;
         }
     }
 }

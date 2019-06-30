7fdca20d51379617616fc40121d8598d5a467b2f
Aniket <asingla@tavisca.in>
2019-07-01 00:39:18 +0530

    Made console calculator and passed tests

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,140 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
-        [Fact]
-        public void DummyTest()
+        private Calculator calculator;
+        public CalculatorFixture()
         {
-            return;
+            calculator = new Calculator();
         }
+
+        private string SendMultipleKey(char[] key)
+        {
+            var i = 0;
+            for (i = 0; i < key.Length - 1; i++)
+                calculator.SendKeyPress(key[i]);
+            return calculator.SendKeyPress(key[i]);
+        }
+
+
+
+        [Fact]
+        public void SingleKeyPressTest()
+        {
+            //simplest test case
+            char key = '5';
+
+            var result = calculator.SendKeyPress(key);
+
+            //Assert
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public void TestConsideringOnlyTwoOperandsForAddOperator()
+        {
+            char[] key = { '1','.', '2', '+', '3', '=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("4.2", result);
+        }
+
+        [Fact]
+        public void DivByZeroForTwoOperands()
+        {
+            char[] key = { '1', '2', '/', '0', '=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("-E-", result);
+        }
+
+        [Fact]
+        public void DoubleDivisionForTwoOperands()
+        {
+            char[] key = { '1','.', '2', '/', '3', '=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("0.4", result);
+        }
+
+        [Fact]
+        public void DoubleDecimalTwoOperands()
+        {
+            char[] key = { '1', '.','.', '2', '/', '3', '=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("0.4", result);
+        }
+
+        [Fact]
+        public void SignInversionTest()
+        {
+            char[] key = { '1', '2', '/','2','s','=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("-6", result);
+        }
+
+        [Fact]
+        public void MultipleZeroesAndDecimalTest()
+        {
+            char[] key = { '0','0', '.','.','2', '/', '2', '=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("0.1", result);
+        }
+
+        [Fact]
+        public void PressingClearTest()
+        {
+            char[] key = { '0', '0', '.', '.', 'C', '1', '2', '/', '2', 's', '=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("-6", result);
+        }
+
+        [Fact]
+        public void FirstTest()
+        {
+            char[] key = { '1', '0', '+', '2', '=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("12", result);
+        }
+
+        [Fact]
+        public void SecondTest()
+        {
+            char[] key = { '1','0', '/', '0', '=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("-E-", result);
+        }
+        [Fact]
+        public void ThirdTest()
+        {
+            char[] key = { '0', '0', '.', '.', '0', '0', '1' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("0.001", result);
+        }
+
+
+        [Fact]
+        public void FourthTest()
+        {
+            char[] key = { '1', '2', '+', '2', 'S', 'S', 'S','=' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("10", result);
+        }
+
+        [Fact]
+        public void FifthTest()
+        {
+            char[] key = { '1','+','2','+','3' ,'+','6','='};
+            var result = SendMultipleKey(key);
+            Assert.Equal("12", result);
+        }
+
+
+        [Fact]
+        public void SixthTest()
+        {
+            char[] key = { '1', '+', '2', '+','3', 'C' };
+            var result = SendMultipleKey(key);
+            Assert.Equal("0", result);
+        }
+
+
+
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,170 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
+        private List<Char> allowedOperators = new List<char>
+        {
+             '-', '+', 'x','X', '/', 's', 'S','c','C'
+        };
 
+        //firstNumber will also contain the result
+        private string firstNumber = "";
+        private string secondNumber = "";
+        private char? operation = null;
+        private int operatorPressed = 0;
+        private bool decFirst = false;
+        private bool decSecond = false;
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            
+            if (IsValid(key))
+            {
+                if (char.IsDigit(key))
+                {
+                    if (operatorPressed == 0 )
+                    {
+                        if (!(firstNumber.Equals("0") && key=='0'))
+                        {
+                            firstNumber += key;
+                        }
+                        
+                                               
+                    }
+                    else
+                    {
+                        if (!(secondNumber.Equals("0")&&key=='0'))
+                        {
+                            secondNumber += key;
+                        }
+                        
+                        return secondNumber;
+                    }
+                    
+                }
+                else if (key.Equals('.'))
+                {
+                    if (operatorPressed == 0 && decFirst==false)
+                    {
+                        if (firstNumber.Length == 0)
+                        {
+                            firstNumber = "0.";
+                        }
+                        else
+                        {
+                            firstNumber += ".";
+                        }
+                        decFirst = true;
+                    }
+                    else if (operatorPressed == 1 && decSecond == false)
+                    {
+                        if (secondNumber.Length == 0)
+                        {
+                            secondNumber = "0.";
+                        }
+                        else
+                        {
+                            secondNumber += ".";
+                        }
+                        decSecond = true;
+                    }
+                }
+                else if (key == 's' || key == 'S')
+                {
+                    if (operatorPressed == 1)
+                    {
+                        secondNumber = (-double.Parse(secondNumber)).ToString();
+                    }
+                    else
+                    {
+                        firstNumber = (-double.Parse(firstNumber)).ToString();
+                    }
+                }
+                else if (key == 'c' || key == 'C')
+                {
+                    firstNumber = "0";
+                    secondNumber = "";
+                    operation = null;
+                    operatorPressed = 0;
+                    decFirst = decSecond = false;
+                }
+                else if (key.Equals('='))
+                {
+                    ComputeResult();
+                    operatorPressed -= 1;
+                }
+                else if (allowedOperators.Contains(key))
+                {
+                    operatorPressed += 1;
+                    //compute previous if any 
+                    if (operatorPressed == 2)
+                    {
+                        ComputeResult();
+                        secondNumber = "";
+                        operatorPressed -= 1;
+                    }
+                    
+                                     
+                    operation = key;
+                    
+                }
+
+
+
+
+
+                return firstNumber;
+            }
+            else
+            {
+                return firstNumber;
+            }
+        }
+
+        private void ComputeResult()
+        {
+            switch (operation)
+            {
+                case '+':
+                    firstNumber = (double.Parse(firstNumber) + double.Parse(secondNumber)).ToString();
+                    break;
+                case '-':
+                    firstNumber = (double.Parse(firstNumber) - double.Parse(secondNumber)).ToString();
+                    break;
+                case 'x':
+                case 'X':
+                    firstNumber = (double.Parse(firstNumber) * double.Parse(secondNumber)).ToString();
+                    break;
+                case '/':
+                    try
+                    {
+                        firstNumber = (double.Parse(firstNumber) / double.Parse(secondNumber)).ToString();
+                        if (firstNumber.Equals("Infinity"))
+                        {
+                            //firstNumber = "-E-";
+                            throw new DivideByZeroException();
+                        }
+                    }
+                    catch (DivideByZeroException)
+                    {
+
+                        firstNumber = "-E-";
+                    }
+                    break;
+            }
+            
+
+        }
+
+        private bool IsValid(char key)
+        {
+            if(char.IsDigit(key) || allowedOperators.Contains(key)|| key=='.'|| key.Equals('='))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }

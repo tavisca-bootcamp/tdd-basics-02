268892d3c9468a105c7281f9acd50cc3522598c4
MayankGandhe <mayank.gandhe@gmail.com>
2019-06-30 19:43:58 +0530

    Final Changes with updated code

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -10,5 +10,132 @@ namespace ConsoleCalculator.Tests
         {
             return;
         }
+        [Fact]
+        public void NumberTest()
+        {
+            var Cal = new Calculator();
+            Cal.SendKeyPress('1');
+            Cal.SendKeyPress('.');
+            Cal.SendKeyPress('2');
+            Cal.SendKeyPress('3');
+            Cal.SendKeyPress('s');
+            string result = Cal.SendKeyPress('*');
+            Assert.Equal("-1.23", result);
+            return;
+        }
+        [Fact]
+        public void DivisionTest()
+        {
+            var Cal = new Calculator();
+            Cal.SendKeyPress('1');
+            Cal.SendKeyPress('3');
+            Cal.SendKeyPress('s');
+            Cal.SendKeyPress('/');
+            Cal.SendKeyPress('2');
+            string result = Cal.SendKeyPress('=');
+            Assert.Equal("-6.5", result);
+            return;
+        }
+        [Fact]
+        public void MultiplicationTest()
+        {
+            var Cal = new Calculator();
+            Cal.SendKeyPress('1');
+            Cal.SendKeyPress('.');
+            Cal.SendKeyPress('3');
+            Cal.SendKeyPress('s');
+            Cal.SendKeyPress('*');
+            Cal.SendKeyPress('2');
+            string result = Cal.SendKeyPress('=');
+            Assert.Equal("-2.6", result);
+            return;
+        }
+        [Fact]
+        public void AdditionTest()
+        {
+            var Cal = new Calculator();
+
+            Cal.SendKeyPress('1');
+            Cal.SendKeyPress('1');
+            Cal.SendKeyPress('2');
+            Cal.SendKeyPress('3');
+            Cal.SendKeyPress('+');
+            Cal.SendKeyPress('2');
+            string result = Cal.SendKeyPress('=');
+
+            Assert.Equal("1125", result);
+            return;
+        }
+        [Fact]
+        public void SubstractionTest()
+        {
+            var Cal = new Calculator();
+            Cal.SendKeyPress('1');
+            Cal.SendKeyPress('.');
+            Cal.SendKeyPress('2');
+            Cal.SendKeyPress('3');
+            Cal.SendKeyPress('s');
+            Cal.SendKeyPress('-');
+            Cal.SendKeyPress('2');
+            string result = Cal.SendKeyPress('=');
+
+            Assert.Equal("-3.23", result);
+            return;
+        }
+        [Fact]
+        public void ClearTest()
+        {
+            var Cal = new Calculator();
+            Cal.SendKeyPress('1');
+            Cal.SendKeyPress('.');
+            Cal.SendKeyPress('2');
+            Cal.SendKeyPress('3');
+            Cal.SendKeyPress('c');
+            string result = Cal.SendKeyPress('*');
+            Assert.Equal("0", result);
+            return;
+        }
+
+        [Fact]
+        public void SwapTest()
+        {
+            var Cal = new Calculator();
+
+            Cal.SendKeyPress('2');
+            Cal.SendKeyPress('3');
+            Cal.SendKeyPress('s');
+            string result = Cal.SendKeyPress('*');
+            Assert.Equal("-23", result);
+            return;
+        }
+        [Fact]
+        public void ErrorTest()
+        {
+            var Cal = new Calculator();
+
+            Cal.SendKeyPress('2');
+            Cal.SendKeyPress('3');
+            Cal.SendKeyPress('/');
+            Cal.SendKeyPress('0');
+            string result = Cal.SendKeyPress('=');
+            Assert.Equal("-E-", result);
+            return;
+        }
+        [Theory]
+        [InlineData("0",'1','2','s','*','0','=')]
+        [InlineData("1230",'1','2','3','*', '1', '0', '=')]
+        [InlineData("-E-",'1','2','3','/','0', '=')]
+        [InlineData("-E-",'1','.','2','3','*','1', '=')]
+        public void ParameterizedTesting(string result, params char[] keys)
+        {
+            var Cal = new Calculator();
+            string actualresult = "";
+            for (int i = 0; i < keys.Length; i++)
+            {
+                actualresult = Cal.SendKeyPress(keys[i]);
+            }
+            Assert.Equal(result,actualresult);
+
+        }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,13 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
-
+        List<double> values = new List<double>();
+        List<char> operations = new List<char>();
+        static int pow = 10;
+        static bool isOperation = false,dotActive = false;
         public string SendKeyPress(char key)
-        {
-            // Add your implementation here.
+        {    
+            if (Char.IsDigit(key))
+            {   if (values.Count == 0)
+                    values.Add(0);
+
+                if (!dotActive)
+                {
+                    values[values.Count - 1] = (values[values.Count - 1] * 10) + Int32.Parse(key.ToString());
+                    pow = 10;
+                }
+                else
+                {
+                    values[values.Count - 1] = values[values.Count - 1] + (Double.Parse(key.ToString()) / pow);
+                    pow *= 10;
+                }
+            }
+            if (key.Equals('-') || key.Equals('+') || key.Equals('/') || key.Equals('*'))
+            {
+                values.Add(0);
+                dotActive = false;
+                operations.Add(key);
+                return values[values.Count - 2].ToString();
+
+            }
+            if (key.Equals('.'))
+                dotActive = true;
+
+            if (Char.ToUpperInvariant(key).Equals('S'))
+            {
+                values[values.Count - 1] *= (-1);
+            }
+            if (Char.ToUpperInvariant(key).Equals('C'))
+            {
+                values.Clear();
+                values.Add(0);
+                operations.Clear();
+                return "0";
+            }
+            if (key.Equals('='))
+            {
+                if (operations.Count == values.Count - 1)
+                {
+                    for (int i = 0; i < operations.Count; i++)
+                    {       if (operations[i].Equals('*'))
+                                values[0] = values[0] * values[1];
+                            if (operations[i].Equals('/'))
+                            {
+                                if (values[1] != 0)
+                                    values[0] = values[0] / values[1];
+                                else
+                                    return "-E-";
+                            }
+                            if (operations[i].Equals('+'))
+                                values[0] = values[0] + values[1];
+                            if (operations[i].Equals('-'))
+                                values[0] = values[0] - values[1];
+                            values.RemoveAt(1);
+                        
+                        
+                    }
+                    return values[0].ToString();
+                }
+               
+                    return "-E-";
+               
+
+
+            }
+            return values[values.Count - 1].ToString();
             throw new NotImplementedException();
         }
     }

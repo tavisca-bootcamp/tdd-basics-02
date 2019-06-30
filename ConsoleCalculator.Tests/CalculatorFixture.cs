356c347214837c15cf3d35f37cd2712145bdf159
tavisca-vshah <vshah@tavisca.in>
2019-06-30 20:27:59 +0530

    Solved Problem

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -10,5 +10,121 @@ namespace ConsoleCalculator.Tests
         {
             return;
         }
+        [Fact]
+        public void TypeOfCalculatorObjectTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsType<Calculator>(calculator);
+            
+        }
+        [Fact]
+        public void TestOfInvalidDigitPassedToSendKeyMethod()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Equal("0", calculator.SendKeyPress('f'));
+        }
+        [Fact]
+        public void TestOfWhiteCharPassedToSendKeyMethod()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Equal("0", calculator.SendKeyPress(' '));
+        }
+        [Fact]
+        public void TestOfMoreThanOneDecimalPointOnFirstOperand()
+        {
+            
+            Assert.Equal("7.1", ProcessEquation("7..1"));
+        }
+        [Fact]
+        public void TestOfMoreThanOneDecimalPointOnSecondOperand()
+        {
+            Assert.Equal("2.3", ProcessEquation("1+1..3="));
+        }
+        [Fact]
+        public void TestOfPerfectEquation()
+        {
+            Assert.Equal("16", ProcessEquation("5+11="));
+        }
+        [Fact]
+        public void TestOfLeadingZeroOnDecimalPoint()
+        {
+            Assert.Equal("0.29", ProcessEquation("0..29="));
+        }
+        [Fact]
+        public void TestOfToggleButton()
+        {
+            Assert.Equal("-5", ProcessEquation("5s"));
+        }
+        [Fact]
+        public void TestOfShortHandOperator()
+        {
+            Assert.Equal("10", ProcessEquation("5+="));
+        }
+        [Fact]
+        public void TestOfShortHandOperatorPlusEquation()
+        {
+            Assert.Equal("10", ProcessEquation("2+3+="));
+        }
+        [Fact]
+        public void TestOfResetCalculator()
+        {
+            Assert.Equal("0", ProcessEquation("2+3+c="));
+        }
+        [Fact]
+        public void TestOfInvalidCharacter()
+        {
+            Assert.Equal("30", ProcessEquation("1aidb5+2tbjd#$@0 - 5="));
+        }
+        [Fact]
+        public void TestOfDecimalPointWorking()
+        {
+            Assert.Equal("17.3", ProcessEquation("7..1 + 10...2 = "));
+        }
+        [Fact]
+        public void TestOfDivideByZero()
+        {
+            Assert.Equal("-E-", ProcessEquation("7/0="));
+        }
+        [Fact]
+        public void TestOfMultiplication()
+        {
+            Assert.Equal("35", ProcessEquation("7*5="));
+        }
+        [Fact]
+        public void TestOfMultiplicationVariant2()
+        {
+            Assert.Equal("70", ProcessEquation("7*5*2="));
+        }
+        [Fact]
+        public void TestOfDivison()
+        {
+            Assert.Equal("25", ProcessEquation("50/2="));
+        }
+        [Fact]
+        public void TestOfMixOperator()
+        {
+            Assert.Equal("3", ProcessEquation("2+3-2="));
+        }
+        [Fact]
+        public void TestOfMixOperatorVariant2()
+        {
+            Assert.Equal("3", ProcessEquation("2+3--2="));
+        }
+        [Fact]
+        public void TestOfDivisonVariant2()
+        {
+            Assert.Equal("1", ProcessEquation("50/2/="));
+        }
+        public string ProcessEquation(string equation)
+        {
+            Calculator calc = new Calculator();
+            string output = "";
+            foreach (char ch in equation)
+            {
+                output = calc.SendKeyPress(ch);
+            }
+            return output;
+        }
+        
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -2,13 +2,142 @@
 
 namespace ConsoleCalculator
 {
+    class InvalidDigitException : Exception
+    {
+        public InvalidDigitException(string message) : base(message)
+        {
+        }
+    }
     public class Calculator
     {
-
+        string operand1 = "0";
+        string operand2 = "";
+        char previousOperator = '+';
+        string outputOnScreen = "0";
+        bool isDecimalOccured = false;
+        string previousOperandResult = "";
+        private void resetCalculator()
+        {
+            operand1 = "0";
+            operand2 = "";
+            previousOperator = '+';
+            outputOnScreen = "0";
+            isDecimalOccured = false;
+            previousOperandResult = "";
+        }
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            string result = "0";
+            try
+            {
+                result = PerformOperationOnKeyPressed(key);
+            }
+            catch (InvalidDigitException)
+            {
+                result = outputOnScreen;
+            }
+            catch (DivideByZeroException)
+            {
+                result = "-E-";
+            }
+            return result;
+        }
+
+        private string PerformOperationOnKeyPressed(char key)
+        {
+            if (InputHandler.isClearKey(key))
+            {
+                resetCalculator();
+                outputOnScreen = "0";
+            }
+            else if (InputHandler.isToggleKey(key))
+            {
+                toggleSign();
+                outputOnScreen = operand2;
+            }
+            else if (InputHandler.isDecimalPoint(key))
+            {
+                if (!isDecimalOccured)
+                {
+                    operand2 = operand2 + key;
+                    isDecimalOccured = true;
+                    outputOnScreen = operand2;
+                }
+            }
+            else if (InputHandler.isValidOperator(key))
+            {
+                isDecimalOccured = false;
+                if (key == '=' && operand2 == "")
+                    operand2 = previousOperandResult;
+                ComputeOperation();
+                outputOnScreen = operand1;
+
+                if (key != '=')
+                {
+                    previousOperator = key;
+                    previousOperandResult = operand1;
+                }
+                else
+                {
+                    previousOperandResult = operand2;
+                }
+                operand2 = "";
+            }
+            else if (InputHandler.isValidDigit(key))
+            {
+                if (key == '0')
+                    HandleInputZero(key);
+                else
+                    operand2 = operand2 + key;
+                outputOnScreen = operand2;
+            }
+            else
+            {
+                throw new InvalidDigitException($"Invalid digit '{key}' pressed");
+            }
+            return outputOnScreen;
+        }
+        private void HandleInputZero(char key)
+        {
+            if (isDecimalOccured)
+            {
+                operand2 = operand2 + key;
+            }
+            else
+            {
+                operand2 = Int32.Parse(operand2 + key).ToString();
+            }
+        }
+        private void toggleSign()
+        {
+            operand2 = (-1 * Double.Parse(operand2)).ToString();
+        }
+        private void ComputeOperation()
+        {
+            if (operand2 == "")
+                return;
+            double tempOperand1 = Double.Parse(operand1);
+            double tempOperand2 = Double.Parse(operand2);
+
+            switch (previousOperator)
+            {
+                case '+':
+                    operand1 = Operation.Add(tempOperand1, tempOperand2).ToString();
+                    break;
+                case '-':
+                    operand1 = Operation.Subtract(tempOperand1, tempOperand2).ToString();
+                    break;
+                case 'X':
+                case '*':
+                case 'x':
+                    operand1 = Operation.Multiply(tempOperand1, tempOperand2).ToString();
+                    break;
+                case '/':
+                    if (operand2 == "0")
+                        throw new DivideByZeroException("Can't Divide By Zero");
+                    operand1 = Operation.Divide(tempOperand1, tempOperand2).ToString();
+                    break;
+            }
         }
     }
 }

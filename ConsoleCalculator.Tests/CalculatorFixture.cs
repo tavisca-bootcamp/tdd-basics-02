cab8dacabd5abed89fe3f640d5f7fdbdc880d7d0
tavisca-vjola <vjola@tavisca.in>
2019-06-30 13:18:24 +0530

    Update CalculatorFixture.cs
    updated test

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,9 +5,19 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
-        [Fact]
-        public void DummyTest()
+        Calculator calc;
+        CalculatorFixture()
         {
+            calc=new Calculator();
+        }
+
+        [Fact]
+        public void SampleTest()
+        {
+            string expr="10+12=";
+            string expected="22";
+            string actual=calc.CalculateExpression(expr);
+            Assert.Equal(expected,actual);
             return;
         }
     }

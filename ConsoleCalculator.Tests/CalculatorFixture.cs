dc2a162e163c8c94c32540698a39a8fdc712ee16
tavisca-acharpe <51903447+tavisca-acharpe@users.noreply.github.com>
2019-07-01 13:28:44 +0530

    Update CalculatorFixture.cs

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -1,14 +1,50 @@
 using System;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ConsoleCalculator.Tests
 {
-    public class CalculatorFixture
+    [TestClass]
+    public class UnitTest1
     {
-        [Fact]
-        public void DummyTest()
+        [TestMethod]
+        public void CheckDoubleAddition()
         {
-            return;
+            // Arrange
+                Double expected =0.02;
+            //Act
+                 Double actual = ConsoleCalculator.Calculator.Calculation(0.01,0.01,'+');
+                 Assert.AreEqual(expected,actual);
         }
+
+        [TestMethod]
+        public void CheckDoubleSubstraction()
+        {
+            // Arrange
+            Double expected = -10;
+            //Act
+            Double actual = ConsoleCalculator.Calculator.Calculation(0, 10, '-');
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckDoubleMultiplication()
+        {
+            // Arrange
+            Double expected = 156.25;
+            //Act
+            Double actual = ConsoleCalculator.Calculator.Calculation(12.5, 12.5, '*');
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CheckDoubleDivision()
+        {
+            // Arrange
+            Double expected = 5;
+            //Act
+            Double actual = ConsoleCalculator.Calculator.Calculation(25.00, 5, '/');
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }

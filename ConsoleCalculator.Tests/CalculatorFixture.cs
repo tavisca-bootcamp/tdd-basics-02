377950c7f6dc5475ff145346454c3417f022a7c6
tavisca-ksingh <ksingh@tavisca.in>
2019-07-01 20:02:46 +0530

    Completed the Calculator Console Progam and test cases

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,108 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
-        [Fact]
-        public void DummyTest()
+        private string GetResults(string expression)
         {
-            return;
+            Calculator calculator = new Calculator();
+            string results = "";
+            foreach(char keys in expression)
+            {
+                results = calculator.SendKeyPress(keys);
+            }
+            calculator.SendKeyPress('c');
+            return results;
         }
+        
+        [Fact]
+        public void BasicTest()
+        {
+            string expr = "10+12=";
+            string expected = "22";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SampleTest1()
+        {
+            string expr = "10+2=";
+            string expected = "12";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SampleTest2DivideByZero()
+        {
+            string expr = "10/0=";
+            string expected = "-E-";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SampleTest3CheckingMultipleDots()
+        {
+            string expr = "00..00..0001";
+            string expected = "0.000001";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void SampleTest4SignToggled()
+        {
+            string expr = "12+2SsS=";
+            string expected = "10";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SampleTest5CheckingMultipleOprends()
+        {
+            string expr = "1+2+3+=";
+            string expected = "12";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SampleTest5CheckingMultipleOprends1()
+        {
+            string expr = "1+2+C";
+            string expected = "0";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void checkingMultipication()
+        {
+            string expr = "2*3*6=";
+            string expected = "36";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void checkingNegative()
+        {
+            string expr = "36sx54=";
+            string expected = "-1944";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Checkingdivision()
+        {
+            string expr = "15.5/3=";
+            string expected = "5.17";
+            string actual = GetResults(expr);
+            Assert.Equal(expected, actual);
+        }
+
+
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -4,11 +4,183 @@ namespace ConsoleCalculator
 {
     public class Calculator
     {
+        static string userPressedKey = "";
+        static string result = "";
+        static string first="";
+        static char operand;
+        static bool flag = false;
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+           if(Char.IsLetter(key))
+            {
+                key = char.ToLower(key);
+
+                if (key == 'x') key = '*';
+
+                switch (key)
+                {
+                    case 'c':
+                        ClearCalculator();
+                        break;
+                    case 's':
+                        ChangedSign();
+                        break;
+                    
+                }
+            }
+           if(char.IsDigit(key))
+            {
+                if(key == '0')
+                {
+                    if(String.IsNullOrEmpty(userPressedKey) || userPressedKey == "0")
+                    {
+                        userPressedKey = "0";
+                    }
+                    else
+                    {
+                            userPressedKey += key;
+                    }
+                }
+                else
+                {
+                    // to remove zero infront of the digit  
+                    if (userPressedKey == "0")
+                    {
+                        userPressedKey = key.ToString();
+                    }
+                    else
+                    {
+                        userPressedKey += key;
+                    }
+                }
+            }
+            else
+            {
+                switch (key)
+                {
+                    case '+':
+                    case '-':
+                    case '/':
+                    case '*':
+                        if (flag == false)
+                        {
+                            setVariable(userPressedKey, key);
+                            flag = true;
+                        }
+                        else
+                        {//if user press multiple operand then it will calculate
+                            return RepeatOpertaion(key);
+                        }
+                        break;
+                    case '=':
+                       // Console.WriteLine($"first : {first} results : {result} userPressedKey : {userPressedKey} operand : {operand}");
+                        if (flag == true && !string.IsNullOrEmpty(result)) userPressedKey = result;
+                        result = PerformsOperation(first,userPressedKey, operand);
+                        userPressedKey = "0";
+                        return result;
+
+                    case '.':
+                        checkReaptationOfDot(key);
+                        break;
+
+                }
+
+            }
+
+            return userPressedKey;
         }
+        //reseting all the values
+        public static void ClearCalculator()
+        {
+             userPressedKey = "0";
+             result = "";
+             first = "";
+            operand = char.MinValue;
+             flag = false;
+            Console.Clear();
+        }
+
+        public static void ChangedSign()
+        {
+            double signNumber = 0.0;
+            if(!string.IsNullOrEmpty(userPressedKey))
+            {
+                double.TryParse(userPressedKey, out signNumber);
+                signNumber = signNumber * -1;
+            }
+            userPressedKey = signNumber.ToString();
+        }
+
+        public static void checkReaptationOfDot(char key)
+        {
+            if(!string.IsNullOrEmpty(userPressedKey))
+            {
+                if(!userPressedKey.Contains("."))
+                {
+                    userPressedKey += key;
+                }
+            }
+            else
+            {
+                userPressedKey = "0";
+            }
+        }
+
+        public static void setVariable(string fst, char symbol)
+        {
+            //intialize the variable for performing the operations
+            first = fst;
+            operand = symbol;
+            userPressedKey = "";
+            
+        }
+
+        public static string RepeatOpertaion(char operation)
+        {
+            if(string.IsNullOrEmpty(result))
+            {
+                result = first;
+            }
+            result = PerformsOperation(result, userPressedKey, operation);
+            userPressedKey = "0";
+            first = result;
+            operand = operation;
+            return result;
+        }
+
+        public static string PerformsOperation(string first,string second, char operation)
+        {
+            double number1, number2, result = 0.0;
+            double.TryParse(first, out number1);
+            double.TryParse(second, out number2);
+
+            switch (operation)
+            {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '/':
+                    if (number2 > 0)
+                    {
+                        result = (number1 / number2);
+                    }
+                    else
+                    {
+                        return "-E-";
+                    }
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+            }
+
+            return result.ToString("0.##");
+
+        }
+
     }
 }

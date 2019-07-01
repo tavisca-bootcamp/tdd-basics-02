adbc365a138a0cbee0906745b9df35c6672c0511
Tanmay Sharma <tsharma@tavisca.in>
2019-07-01 14:10:11 +0530

    tdd-basics2

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,81 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
-        [Fact]
-        public void DummyTest()
+        Calculator cal;
+        public CalculatorFixture()
         {
-            return;
+            cal = new Calculator();
         }
+
+        [Fact]
+        public void tenplus2()
+        {
+            string s = "10+2=";
+            string r = "";
+            foreach(char c in s)
+            {
+                r = cal.SendKeyPress(c);
+                if (r.Equals("-E-"))
+                    break;
+            }
+            Assert.Equal("12", r);
+        }
+
+        [Fact]
+        public void DivideByZero()
+        {
+            string s = "10/0=";
+            string r = "";
+            foreach (char c in s)
+            {
+                r = cal.SendKeyPress(c);
+                if (r.Equals("-E-"))
+                    break;
+            }
+            Assert.Equal("-E-", r);
+        }
+
+        [Fact]
+        public void MoreThanOneDecimalPoint()
+        {
+            string s = "00..001";
+            string r = "";
+            foreach (char c in s)
+            {
+                r = cal.SendKeyPress(c);
+                if (r.Equals("-E-"))
+                    break;
+            }
+            Assert.Equal("0.001", r);
+        }
+
+        [Fact]
+        public void useOfS()
+        {
+            string s = "12+2SSS=";
+            string r = "";
+            foreach (char c in s)
+            {
+                r = cal.SendKeyPress(c);
+                if (r.Equals("-E-"))
+                    break;
+            }
+            Assert.Equal("10", r);
+        }
+
+        [Fact]
+        public void UseOfC()
+        {
+            string s = "1+2+C";
+            string r = "";
+            foreach (char c in s)
+            {
+                r = cal.SendKeyPress(c);
+                if (r.Equals("-E-"))
+                    break;
+            }
+            Assert.Equal("0", r);
+        }
+
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,156 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleCalculator
 {
+
     public class Calculator
     {
+        string s = "";
+        char op = '+';
+        decimal num = 0;
+        decimal result = 0;
+        int f = 0, d=0;
+        char[] opr = { '+', '-', '*', '/' };
+
+       
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            HashSet<char> hs = new HashSet<char>(opr);
+
+            if ( (int)key>=48 && (int)key<=57 )
+            {
+                f = 0;
+                int n = (int)key - 48;
+                if (s.Equals("0"))
+                    s = "";
+                s = s + n;
+                return s;
+            }
+            else if (hs.Contains(key))
+            {
+                if (f != 0)
+                    return "-E-";
+                f = 1;
+                d = 0;
+                decimal n1 = 0;
+                n1 = decimal.Parse(s);
+                if (op == '+')
+                {
+                    num = num + n1;
+                }
+                else if (op == '-')
+                {
+                    num = num - n1;
+                }
+                else if (op == '*')
+                {
+                    num = num * n1;
+                }
+                else if (n1 == 0)
+                {
+                    return "-E-";
+                }
+                else
+                {
+                    num = num / n1;
+                }
+
+                if (key =='+' || key == '-')
+                {
+                    result = result + num;
+                    num = 0;
+                }
+                op = key;
+
+                s = "";
+
+                decimal x = result + num;
+                string y = "" + x;
+                return y;
+            }
+
+            if (key == '.')
+            {
+                if (d==0)
+                {
+                    if (s.Length == 0)
+                        s = "0";
+                    s = s + ".";
+                    d = 1;
+                }
+                return s;
+            }
+
+            if (key == 'S')
+            {
+                if (s.Length == 0)
+                {
+                    result = 0-result;
+                    return ("" + result);
+                }
+                if (s[0] == '-')
+                    s = s.Substring(1);
+                else if (!s.Equals("0"))
+                    s = "-" + s;
+                return s;
+            }
+
+
+
+            if (key == '=')
+            {
+                if (f != 0)
+                {
+                    return "-E-";
+                }
+                d = 0;
+                decimal n1 = 0;
+                if (s.Length > 0)
+                    n1 = decimal.Parse(s);
+                if (op == '+')
+                {
+                    result = result + n1;
+                }
+                else if (op == '-')
+                {
+                    result = result - n1;
+                }
+                else if (op == '*')
+                {
+                    num = num * n1;
+                    result += num;
+                    num = 0;
+                }
+                else if (n1 == 0)
+                {
+                    return "-E-";
+                }
+                else
+                {
+                    num = num / n1;
+                    result += num;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                    num = 0;
+                }
+                op = '+';
+                return ("" + result);
+                
+            }
+
+            if (key == 'C')
+            {
+                s = "";
+                op = '+';
+                num = 0;      
+                result = 0;                                                                                                                                                                                                                           
+                f = 1                                                                                                                                          ;
+                d = 0;
+                return "0";
+            }
+
+
+            return "-E-";
         }
     }
 }

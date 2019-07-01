a6774c454cb9c783f48bc94291b065daa3e813f8
tavisca-apandey <apandey@tavisca.in>
2019-07-01 12:26:02 +0530

    Assignment 3.2

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,60 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
-        [Fact]
-        public void DummyTest()
-        {
-            return;
+        Calculator c = new Calculator();
+            
+            [Fact]
+            public void BasicOperationTest()
+            {
+                Assert.Equal("12",Evaluation("10+2="));
+            }
+
+            [Fact]
+            public void DivideByZeroTest()
+            {
+
+                Assert.Equal("-E-",Evaluation("10/0="));
+            }
+
+            [Fact]
+            public void DecimalFormatTest()
+            {
+                Assert.Equal("0.001",Evaluation("00..001"));
+            }
+
+            [Fact]
+            public void MultiplicationTest()
+            {
+                Assert.Equal("20",Evaluation("5x2X2="));
+            }
+
+            [Fact]
+            public void TogglingTest()
+            {
+                Assert.Equal("8",Evaluation("10+2S="));
+            }
+
+            [Fact]
+            public void ComplexOperationTest()
+            {
+                Assert.Equal("6",Evaluation("1+2+3+"));
+            }
+
+            [Fact]
+            public void ClearConsoleTest()
+            {
+                Assert.Equal("0",Evaluation("1+2+C"));
+            }
+
+            public string Evaluation(string equation)
+            {
+                string display = "";
+                foreach(char term in equation)
+                {
+                    display = c.SendKeyPress(term);
+                }
+                return display;
+            }
         }
     }
-}
+
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,109 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
+        private char currentOperator='\0';
+	    private string store="0";
+        private string currentValue="0";
+	    private Queue<int> value_store = new Queue<int>(); 
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            
+		    if(key != '+' && key !='-' && key !='x' && key !='X' && key !='/' && key != '=' && key != 'c' && key != 'C' && key != 's' && key != 'S')
+		    {  
+                if(currentValue=="0" && key =='0')
+                        currentValue="0";
+                else
+                {
+                    if(key=='.')
+                    {
+                        if(currentValue.Contains("."))
+                            return currentValue;
+                        else
+                        {
+                            currentValue=currentValue+key;
+                            store=store+key;
+
+                        }
+                    }
+                    else
+                    {
+                        currentValue=store+key;
+                        store=store+key;
+                    }
+                }
+		    }
+		    else if(key == 'c' || key == 'C')
+		    {
+                currentOperator='\0';
+                store="";
+                value_store.Clear();
+                currentValue="0";
+                
+		    }
+            else
+            {
+                if (currentOperator == '\0')
+                {
+                    currentOperator=key;
+                    currentValue=store;
+                    int currentValueInt;
+                    bool ifSuccess = int.TryParse(store, out currentValueInt);
+                    store="";
+                    value_store.Enqueue(currentValueInt);
+                }
+                else if (value_store.Count==1)
+                {
+                    int previousValue=value_store.Dequeue();
+                    int presentValue, newValue;
+                    bool ifSuccess = int.TryParse(store, out presentValue);
+                    string ans="";
+                    if(key=='=')
+                        ans=performCalculation(previousValue, presentValue, currentOperator);
+                    else
+                        ans=performCalculation(previousValue, presentValue, key);
+                    currentValue=ans;
+                    bool ifTrue=int.TryParse(ans, out newValue);
+                    value_store.Enqueue(newValue);
+                    store="";
+                }
+            }
+		return currentValue;
+        }
+
+        public string performCalculation(int previousValue, int presentValue, char key)
+        {
+            int result=0;
+            switch(key)
+            {
+                case '+':
+                    result=previousValue+presentValue;
+                    return result.ToString();
+                case '-':
+                    result=previousValue-presentValue;
+                    return result.ToString();
+                case 'x':
+                case 'X':
+                    result=previousValue*presentValue;
+                    return result.ToString();
+                case '/':
+                    if(presentValue==0)
+                        return "-E-";
+                    else
+                    {
+                        double resultDivide=(double)previousValue/(double)presentValue;
+                        return resultDivide.ToString();
+                    }
+                case 's':
+                case 'S':
+                    result=previousValue+(presentValue*-1);
+                    return result.ToString();  
+            }
+            return "-E-";
         }
     }
 }

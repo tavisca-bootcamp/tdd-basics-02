6cdd427098d882acb403aa29d12f7d6ca47f3ec6
SHIVAM RAJ <51924685+tavisca-sraj@users.noreply.github.com>
2019-07-01 09:04:47 +0530

    TDD-Basics-02 Implemented

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,81 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+        Calculator calci = new Calculator();
+        string query = "";
+        string actualResult = "";
         [Fact]
-        public void DummyTest()
+        public void TestDivisibilityByZero()
         {
-            return;
+            query = "1/0=";
+			string expectedResult = "-E-";
+            actualResult = FetchOutput(query);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void TestForClearScreen()
+        {
+            query = "1/99=c";
+			string expectedResult = "0";
+            actualResult = FetchOutput(query);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void IsValidOutput()
+        {
+            query = "18x2-20+1/1=";
+			string expectedResult = "17";
+            actualResult = FetchOutput(query);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void TestCaseSensitivityForSign()
+        {
+            query = "1+2S=";
+			string expectedResult = "-1";
+            actualResult = FetchOutput(query);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void TestCaseSensitivityForClear()
+        {
+            query = "1+2sS=c1+2C";
+			string expectedResult = "0";
+            actualResult = FetchOutput(query);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void TestCaseSensitivityForMuliplication()
+        {
+            query = "2x2X2=";
+			string expectedResult = "8";
+            actualResult = FetchOutput(query);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void TestPositiveNegativeNumbers()
+        {
+            query = "1+-1=";
+			string expectedResult = "0";
+            actualResult = FetchOutput(query);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+
+        public string FetchOutput(string query) 
+		{
+            string temp = "";
+            foreach (char input in query)
+            {
+                temp = calci.SendKeyPress(input);
+            }
+            return temp;
         }
     }
 }

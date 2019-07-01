e7b4a194b141ecef47d52dc0cb7b77059f3536cd
Venom <saurabhnarhe123@gmail.com>
2019-07-01 12:28:35 +0530

    solution done

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -1,14 +1,132 @@
-using System;
 using Xunit;
 
 namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
-        [Fact]
-        public void DummyTest()
+        Calculator C = new Calculator();
+        string equation = "";
+
+        private string tester(string eq)
         {
-            return;
+            string res = "";
+            foreach (char ch in eq)
+                res = C.SendKeyPress(ch);
+            return res;
         }
+
+        [Fact]
+        public void SingleDigitTest()
+        {
+            Assert.Equal("1", tester("1"));
+        }
+
+        [Fact]
+        public void MultipleZerosTest()
+        {
+            Assert.Equal("0", tester("0000"));
+        }
+
+        [Fact]
+        public void MultipleZerosAfterDecimalPointTest()
+        {
+            Assert.Equal("1.000", tester("1.000"));
+        }
+
+        [Fact]
+        public void MultipleDecimalPointTest()
+        {
+            Assert.Equal("1.000", tester("1..000"));
+        }
+
+        [Fact]
+        public void SignToggleTest()
+        {
+            Assert.Equal("-12", tester("12s"));
+        }
+
+        [Fact]
+        public void ClearTest()
+        {
+            Assert.Equal("0", tester("12c"));
+        }
+
+        [Fact]
+        public void InvalidInputsTest()
+        {
+            Assert.Equal("1", tester("12-j^&11="));
+        }
+
+        [Fact]
+        public void ValidMultipleDecimalPointTest()
+        {
+            equation = "1.1+2.3=";
+            Assert.Equal("3.4", tester(equation));
+        }
+
+        [Fact]
+        public void ValidDivisionTest()
+        {
+            equation = "19200/2=";
+            Assert.Equal("9600", tester(equation));
+        }
+
+        [Fact]
+        public void MultiplicationTest()
+        {
+            equation = "19200x0=";
+            Assert.Equal("0", tester(equation));
+        }
+
+        [Fact]
+        public void SubtractionTest()
+        {
+            equation = "19200-0=";
+            Assert.Equal("19200", tester(equation));
+        }
+
+
+        [Fact]
+        public void Equation1Test()
+        {
+            equation = "10+2=";
+            Assert.Equal("12", tester(equation));
+        }
+
+        [Fact]
+        public void Equation2Test()
+        {
+            equation = "10/0=";
+            Assert.Equal("-E-", tester(equation));
+        }
+
+        [Fact]
+        public void Equation3Test()
+        {
+            equation = "00.001";
+            Assert.Equal("0.001", tester(equation));
+        }
+
+        [Fact]
+        public void Equation4Test()
+        {
+            equation = "12+2SSS=";
+            Assert.Equal("10", tester(equation));
+        }
+
+        [Fact]
+        public void Equation5Test()
+        {
+            equation = "1+2+C";
+            Assert.Equal("0", tester(equation));
+        }
+
+        [Fact]
+        public void Equation6Test()
+        {
+            equation = "1+2+3+=";
+            Assert.Equal("6", tester(equation));
+        }
+
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,108 @@
 ﻿using System;
-
 namespace ConsoleCalculator
 {
     public class Calculator
     {
-
+        private string operand1 = "0";
+        private string operand2 = "";
+        private char Operator = '+';
+        private bool isDecimalEncountered = false;
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            string result = "0";
+
+            try
+            {
+                if (key.ToString().ToUpper() == "C")
+                {
+                    reset();
+                }
+                else if (key.ToString().ToUpper() == "S")
+                {
+                    toggleSign();
+                    result = operand2;
+                }
+                else if (key == '.' && !isDecimalEncountered)
+                {
+                    operand2 = operand2 + key;
+                    isDecimalEncountered = true;
+                    result = operand2;
+                }
+                else if (isDigit(key))
+                {
+                    if (key == '0' && !isDecimalEncountered)
+                        operand2 = Int32.Parse(operand2 + key).ToString();
+                    else
+                        operand2 = operand2 + key;
+                    result = operand2;
+                }
+                else if (isOperator(key))
+                {
+                    isDecimalEncountered = false;
+                    performPreviousOperation();
+                    result = operand1;
+                    if (key != '=')
+                        Operator = key;
+                    operand2 = "";
+                }
+
+            }
+            catch (DivideByZeroException e)
+            {
+                result = "-E-";
+            }
+
+            return result;
         }
+
+        private bool isDigit(char key)
+        {
+            return (key <= '9' && key >= '0') ? true : false;
+        }
+
+        private bool isOperator(char key)
+        {
+            return (key == '+' || key == '-' || key == 'x' || key == 'X' || key == '/' || key == '=') ? true : false;
+        }
+
+        private void reset()
+        {
+            operand1 = "0";
+            operand2 = "";
+            Operator = '+';
+            isDecimalEncountered = false;
+        }
+
+        private void toggleSign()
+        {
+            double d = -Double.Parse(operand2);
+            operand2 = d.ToString();
+        }
+
+        private void performPreviousOperation()
+        {
+            if (operand2 == "")
+                return;
+            switch (Operator)
+            {
+                case '+':
+                    operand1 = (Double.Parse(operand1) + Double.Parse(operand2)).ToString();
+                    break;
+                case '-':
+                    operand1 = (Double.Parse(operand1) - Double.Parse(operand2)).ToString();
+                    break;
+                case '/':
+                    if (operand2 == "0")
+                        throw new DivideByZeroException();
+                    operand1 = (Double.Parse(operand1) / Double.Parse(operand2)).ToString();
+                    break;
+                case 'x':
+                case 'X':
+                    operand1 = (Double.Parse(operand1) * Double.Parse(operand2)).ToString();
+                    break;
+                
+            }
+        }
+
     }
 }

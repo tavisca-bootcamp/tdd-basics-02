4ac17469dd71f3ba30b0aaa03616121c9a0a849d
Stark <shravanramdurg45@gmail.com>
2019-07-01 18:07:35 +0530

    Problem #2

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -4,10 +4,156 @@ namespace ConsoleCalculator
 {
     public class Calculator
     {
+        private string displayChar = "";
+        private float result = float.MaxValue;
+        private char previousOperator = '~';
+        private int flag = 0;
+
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
+            switch (key)
+            {
+                case '0':   if (!displayChar.Equals("0"))
+                            {
+                                displayChar += key;
+                            }
+                    return displayChar;
+
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':   displayChar += key;
+                    return displayChar;
+
+                case '.':   int temp;
+                    if (int.TryParse(displayChar,out temp)==true)           //Checks if the displayed character is a number
+                    {      
+                        displayChar += key;
+                    }
+                    return displayChar;
+
+                case 'c':
+                case 'C':   displayChar = "";
+                    result = float.MaxValue;
+                    flag = 0;
+                    return "0";
+
+                case 's':
+                case 'S':   int temp1;
+                    if(int.TryParse(displayChar,out temp1) == true)
+                    {
+                        displayChar = (-temp1).ToString();
+                    }
+                    else
+                    {
+                        displayChar = (-float.Parse(displayChar)).ToString();
+                    }
+                    return displayChar;
+
+                case '+':
+                    if (flag == 0 && previousOperator!='~')
+                    {
+                        flag = 1;
+                        SendKeyPress(previousOperator);
+                    }
+                    else
+                    {
+                        if (result != float.MaxValue)
+                            result += float.Parse(displayChar);     //adding 2nd number to result
+                        else
+                            result = float.Parse(displayChar);      //storing 1st number in result
+
+                        previousOperator = '+';
+                        flag = 0;
+                        displayChar = "";
+                    }
+
+                    return result.ToString();
+
+                case '-':
+                    if (flag == 0 && previousOperator != '~')
+                    {
+                        flag = 1;
+                        SendKeyPress(previousOperator);
+                    }
+                    else
+                    {
+                        if (result != float.MaxValue)
+                            result -= float.Parse(displayChar);     //Subtracting 2nd number from result
+                        else
+                            result = float.Parse(displayChar);      //storing 1st number in result
+
+                        previousOperator = '-';
+                        flag = 0;
+                        displayChar = "";
+                    }
+
+                    return result.ToString();
+
+
+                case 'X':
+                case 'x':
+                    if (flag == 0 && previousOperator != '~')
+                    {
+                        flag = 1;
+                        SendKeyPress(previousOperator);
+                    }
+                    else
+                    {
+                        if (result != float.MaxValue)
+                            result *= float.Parse(displayChar);
+                        else
+                            result = float.Parse(displayChar);
+
+                        previousOperator = 'x';
+                        displayChar = "";
+                        flag = 0;
+                    }
+                    return result.ToString();
+
+
+                case '/':
+                    if (flag == 0 && previousOperator != '~')
+                    {
+                        flag = 1;
+                        if (SendKeyPress(previousOperator) == "-E-")
+                            return "-E-";
+                    }
+                    else
+                    {
+                        if (result != float.MaxValue)
+                        {
+                            if (float.Parse(displayChar) != 0)
+                                result /= float.Parse(displayChar);
+                            else
+                                return "-E-";
+                        }
+                        else
+                            result = float.Parse(displayChar);
+
+                        previousOperator = '/';
+                        displayChar = "";
+                        flag = 0;
+                    }
+                    return result.ToString();
+
+                case '=':
+                    if (displayChar == "")
+                    {
+                        displayChar = result.ToString();
+                    }
+                    return SendKeyPress(previousOperator);
+
+                default: return displayChar;
+            }
+            
+
             throw new NotImplementedException();
         }
     }

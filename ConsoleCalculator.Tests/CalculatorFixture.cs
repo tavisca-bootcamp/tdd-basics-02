016de5a035cc92ea8ac0a62c4af97b6738c064a5
SHIVANG HARMILAPI <shivangkishanharmilapi@gmail.com>
2019-07-02 18:33:32 +0530

    Updated CalculatorFixture.cs

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -6,9 +6,78 @@ namespace ConsoleCalculator.Tests
     public class CalculatorFixture
     {
         [Fact]
-        public void DummyTest()
+        public void Test1()
         {
-            return;
+            Calculator.SendKeyPress('C');
+            Calculator.SendKeyPress('1');
+            Calculator.SendKeyPress('0');
+            Calculator.SendKeyPress('+');
+            Calculator.SendKeyPress('2');
+            Assert.Equal("12", Calculator.SendKeyPress('='));
+
+
+        }
+
+        [Fact]
+        public void Test2()
+        {
+         
+            Calculator.SendKeyPress('1');
+            Calculator.SendKeyPress('0');
+            Calculator.SendKeyPress('/');
+            Calculator.SendKeyPress('0');
+            Assert.Equal("-E-",Calculator.SendKeyPress('='));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Calculator.SendKeyPress('C');
+            Calculator.SendKeyPress('0');
+            Calculator.SendKeyPress('0');
+            Calculator.SendKeyPress('.');
+            Calculator.SendKeyPress('.');
+            Calculator.SendKeyPress('0');
+            Calculator.SendKeyPress('0');    
+            Assert.Equal("0.001", Calculator.SendKeyPress('1'));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Calculator.SendKeyPress('C');
+            Calculator.SendKeyPress('1');
+            Calculator.SendKeyPress('2');
+            Calculator.SendKeyPress('+');
+            Calculator.SendKeyPress('2');
+            Calculator.SendKeyPress('S');
+            Calculator.SendKeyPress('s');
+            Calculator.SendKeyPress('S');
+            Assert.Equal("10", Calculator.SendKeyPress('='));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Calculator.SendKeyPress('C');
+            Calculator.SendKeyPress('1');
+            Calculator.SendKeyPress('+');
+            Calculator.SendKeyPress('2');
+            Calculator.SendKeyPress('+');
+            Calculator.SendKeyPress('3');
+            Calculator.SendKeyPress('+');
+            Assert.Equal("12", Calculator.SendKeyPress('='));
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            Calculator.SendKeyPress('C');
+            Calculator.SendKeyPress('1');
+            Calculator.SendKeyPress('+');
+            Calculator.SendKeyPress('2');
+            Calculator.SendKeyPress('+');
+            Assert.Equal("0", Calculator.SendKeyPress('c'));
         }
     }
 }

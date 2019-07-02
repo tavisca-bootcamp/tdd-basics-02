4693b3a02e3c184e9b6aa1ed05062040f90942e5
tavisca-vvinod <vvinod@tavisca.in>
2019-07-02 13:14:39 +0530

    Added all functions and tests

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,6 +5,164 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
-        
+        Calculator calculator;
+
+        private void setUp()
+        {
+            this.calculator = new Calculator();
+        }
+
+        [Fact]
+        public void ProcessSingleDigitDecimalInput()
+        {
+            setUp();
+            calculator.PressSingleKey('5');
+            Assert.Equal("5", calculator.ShowDisplayValue());
+            return;
+        }
+
+        [Fact]
+        public void TestMultiDigitDecimalInput()
+        {
+            setUp();
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('5');
+            Assert.Equal("55", calculator.ShowDisplayValue());
+            return;
+        }
+
+        [Fact]
+        public void TestBinarySum()
+        {
+            setUp();
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('4');
+            calculator.PressSingleKey('+');
+            calculator.PressSingleKey('2');
+            calculator.PressSingleKey('3');
+            calculator.PressSingleKey('=');
+            Assert.Equal("77", calculator.DisplayValue);
+        }
+
+        [Fact]
+        public void TestBinaryDiffrence()
+        {
+            setUp();
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('4');
+            calculator.PressSingleKey('-');
+            calculator.PressSingleKey('2');
+            calculator.PressSingleKey('3');
+            calculator.PressSingleKey('=');
+            Assert.Equal("31", calculator.DisplayValue);
+        }
+
+        [Fact]
+        public void TestBinaryDivision()
+        {
+            setUp();
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('/');
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('=');
+            Assert.Equal("10", calculator.DisplayValue);
+        }
+
+        [Fact]
+        public void TestBinaryMultiPlication()
+        {
+            setUp();
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('x');
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('=');
+            Assert.Equal("250", calculator.DisplayValue);
+        }
+
+        [Fact]
+        public void TestResetCalculator()
+        {
+            setUp();
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('/');
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('=');
+            calculator.PressSingleKey('c');
+            Assert.Equal("0", calculator.DisplayValue);
+            Assert.Equal(0, calculator.CurrentOperand);
+            Assert.Equal('\0', calculator.CurrentOperator);
+            Assert.Equal(0, calculator.Result);
+        }
+
+
+        [Fact]
+        public void DivideByZeroDisplayError()
+        {
+            setUp();
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('/');
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('=');
+            Assert.Equal("-E-", calculator.DisplayValue);
+        }
+
+        [Fact]
+        public void ChangeOperandSign()
+        {
+            setUp();
+            calculator.PressSingleKey('5');
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('/');
+            calculator.PressSingleKey('2');
+            calculator.PressSingleKey('3');
+            calculator.PressSingleKey('s');
+            Assert.Equal("-23", calculator.DisplayValue);
+        }
+
+        [Fact]
+        public void EqualToAfterAddOperandDoublesDisplayValue()
+        {
+            setUp();
+            calculator.PressSingleKey('1');
+            calculator.PressSingleKey('+');
+            calculator.PressSingleKey('2');
+            calculator.PressSingleKey('+');
+            calculator.PressSingleKey('3');
+            calculator.PressSingleKey('+');
+            calculator.PressSingleKey('=');
+            Assert.Equal("12", calculator.DisplayValue);
+        }
+
+        [Fact]
+        public void InputMultipleZeroBeforeDecimalDisplaysSingleZero()
+        {
+            setUp();
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('0');
+            Assert.Equal("0", calculator.DisplayValue);
+            calculator.PressSingleKey('+');
+            calculator.PressSingleKey('1');
+            calculator.PressSingleKey('=');
+            Assert.Equal("1", calculator.DisplayValue);
+        }
+
+        [Fact]
+        public void AfterDecimalDisplaysAllValuesIncludingZeros()
+        {
+            setUp();
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('.');
+            calculator.PressSingleKey('0');
+            calculator.PressSingleKey('0');
+            Assert.Equal("0.00", calculator.DisplayValue);
+            calculator.PressSingleKey('1');
+            Assert.Equal("0.001", calculator.DisplayValue);
+        }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -4,7 +4,6 @@ namespace ConsoleCalculator
 {
     public class Calculator
     {
-
         private char currentOperator;
         private decimal result;
         private string displayValue;
@@ -17,7 +16,6 @@ namespace ConsoleCalculator
         public char[] Operators { get => operators; set => operators = value; }
         public decimal CurrentOperand { get => currentOperand; set => currentOperand = value; }
         public char CurrentOperator { get => currentOperator; set => currentOperator = value; }
-
         public void PressSingleKey(char key)
         {
             if (IsDigit(key))
@@ -30,19 +28,20 @@ namespace ConsoleCalculator
                 this.HandleOperator(key);
             }
         }
+        //Function checks for a digit entered
         public void HandleDigit(char key)
         {
-            if (!string.IsNullOrEmpty(this.DisplayValue) && this.DisplayValue.Contains("."))
+            if (!string.IsNullOrEmpty(this.DisplayValue) && this.DisplayValue.Contains("."))//decimal number after decimal point
             {
                 this.SetDisplayValue(this.DisplayValue + key);
             }
-            else
+            else//whole number or decimal number before decimal point
             {
                 this.CurrentOperand = decimal.Parse(this.CurrentOperand.ToString() + key);
                 this.SetDisplayValue(this.CurrentOperand.ToString());
             }
         }
-
+        //handling keys belonging to operator array initialised in the beginning
         public void HandleOperator(char key)
         {
             switch (key)
@@ -71,7 +70,6 @@ namespace ConsoleCalculator
                     break;
             }
         }
-
         public void HandleArithmeticOperator(char key)
         {
             if (this.CurrentOperator == '\0')
@@ -80,13 +78,90 @@ namespace ConsoleCalculator
                 this.Result = this.CurrentOperand;
                 this.CurrentOperand = 0;
             }
-            else
+            else 
             {
                 this.PerformArithmeticOperation(key);
             }
 
         }
+        //computation of operators
+        public void PerformArithmeticOperation(char key)
+        {
+            switch (this.CurrentOperator)
+            {
+                case '+':
+                    if (this.CurrentOperand > 0)
+                    {
+                        this.SetDisplayValue((this.Result += this.CurrentOperand).ToString());
+                    }
+                    else
+                    {
+                        this.SetDisplayValue((this.Result *= 2).ToString());
+                    }
 
+                    break;
 
+                case '-':
+                    this.SetDisplayValue((this.Result -= this.CurrentOperand).ToString());
+                    break;
+
+                case '/':
+                    if (CurrentOperand == 0)
+                    {
+                        this.SetDisplayValue("-E-");
+                    }
+                    else
+                    {
+                        this.SetDisplayValue((this.Result /= this.CurrentOperand).ToString());
+                    }
+
+                    break;
+
+                case 'x':
+                    this.SetDisplayValue((this.Result *= this.CurrentOperand).ToString());
+                    break;
+
+                default:
+                    break;
+            }
+            this.CurrentOperator = key;
+            this.CurrentOperand = 0;
+        }
+        //togglesign is called when 's' is pressed to negate the current sign
+        public void ToggleSign()
+        {
+            this.CurrentOperand = decimal.Negate(this.CurrentOperand);
+            this.SetDisplayValue(this.CurrentOperand.ToString());
+        }
+        //sets display value to current result
+        public void SetDisplayValue(string dispValue)
+        {
+            this.DisplayValue = dispValue;
+        }
+        //resets the values of operators and operands and display to 0
+        public void Reset()
+        {
+            this.CurrentOperand = 0;
+            this.Result = 0;
+            this.DisplayValue = 0.ToString();
+            this.CurrentOperator = '\0';
+        }
+        //displays current result
+        public string ShowDisplayValue()
+        {
+            return this.DisplayValue;
+        }
+        //returns true if the current key pressed belongs to digit array 
+        //initialised in the beginning
+        public bool IsDigit(char inputKey)
+        {
+            return Array.IndexOf(this.Digits, inputKey) > -1;
+        }
+        //returns true if the current key pressed belongs to operator array 
+        //initialised in the beginning
+        public bool IsOperator(char inputKey)
+        {
+            return Array.IndexOf(this.Operators, inputKey) > -1;
+        }
     }
 }

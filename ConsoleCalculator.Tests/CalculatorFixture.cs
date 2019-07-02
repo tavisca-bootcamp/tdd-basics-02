8f307d4d2ca2c10edcf4aaa1f394bd85d264799a
rajat <ryadav@tavisca.in>
2019-07-02 11:15:01 +0530

    Added method to check sum of two binary numbers

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -27,5 +27,16 @@ namespace ConsoleCalculator.Tests
             Assert.Equal("79", _calculator.ShowDisplayValue());
         }
 
+        [Fact]
+        public void TestSum()
+        {
+            _calculator.SendKeyPress('9');
+            _calculator.SendKeyPress('8');
+            _calculator.SendKeyPress('+');
+            _calculator.SendKeyPress('6');
+            _calculator.SendKeyPress('5');
+            _calculator.SendKeyPress('=');
+            Assert.Equal("163", _calculator.ShowDisplayValue());
+        }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -5,11 +5,95 @@ namespace ConsoleCalculator
     public class Calculator
     {
         public string displayValue;
+        public char[] operators = { '.', '+', '-', '/', 'x', 's', 'c', '=' };
+        public decimal currentOperand=0;
+        public char currentOperator;
+        public decimal result;
 
-        public string SendKeyPress(char key)
+    public string SendKeyPress(char key)
         {
-            displayValue += key.ToString();
-            return displayValue;
+            if (Char.IsDigit(key))
+            {
+                DigitHandle(key);
+            }
+
+            if (IsOperator(key))
+            {
+                OperatorHandle(key);
+            }
+
+            return ShowDisplayValue();
+        }
+
+        public void DigitHandle(char key)
+        {
+            currentOperand = decimal.Parse(currentOperand.ToString() + key);
+            if (currentOperand > 0)
+            {
+                SetDisplayValue(currentOperand.ToString());
+            }
+            else
+            {
+                SetDisplayValue(displayValue + key);
+            }
+
+        }
+
+        public void OperatorHandle(char key)
+        {
+            switch (key)
+            {
+                case '+':
+                    SetDisplayValue("+");
+                    HandleArithmeticOperation(key);
+                    break;
+                case '-':
+                case '/':
+                case 'x':
+                case '=':
+                    HandleArithmeticOperation(key);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void HandleArithmeticOperation(char key)
+        {
+            if (currentOperator == '\0')
+            {
+                currentOperator = key;
+                result = currentOperand;
+                currentOperand = 0;
+            }
+            else
+            {
+                PerformsArithmeticOperation(key);
+            }
+        }
+
+        public void PerformsArithmeticOperation(char key)
+        {
+            switch (currentOperator)
+            {
+                case '+':
+                    result += currentOperand;
+                    SetDisplayValue(result.ToString());
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        public bool IsOperator(char key)
+        {
+            return Array.Exists(operators, element => element == key);
+        }
+
+        public void SetDisplayValue(string value)
+        {
+            displayValue = value;
         }
 
         public string ShowDisplayValue()

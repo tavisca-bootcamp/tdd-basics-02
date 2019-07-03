8d095d7f6014c2a32f1918545ec41444070528e7
Raja Sachin <tavisca-rsachin>
2019-07-03 12:29:08 +0530

    First Commit

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,159 @@
 ﻿using System;
+using System.Collections;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
+        enum LastKey
+        {
+            Digit,
+            Operator,
+            Letter,
+            Dot
+        }
+
+        enum OperandSwitch
+        {
+            OperandA,
+            OperandB
+        }
+
+        private string onConsoleOutput = "0";
+
+        private LastKey lastKey = LastKey.Digit;
+        private OperandSwitch operandSwitch = OperandSwitch.OperandA ;
+
+        private char lastKeyValue = '0';
+        private char lastOperator = '#';
+
+        private string operandA;
+        private string operandB;
+        
+        private void Reset()
+        {
+            onConsoleOutput = "0";
+            lastKey = LastKey.Digit;
+            operandSwitch = OperandSwitch.OperandA;
+
+            lastKeyValue = '0';
+            lastOperator = '#';
+        }
+        
+        private void Result()
+        {
+            string result = "-E-";
+            double a = double.Parse(operandA);
+            double b = double.Parse(operandB);
+
+            switch (lastOperator)
+            {
+                case '+':
+                    result = (a + b).ToString();
+                    break;
+                case '-':
+                    result = (a - b).ToString();
+                    break;
+                case 'x':
+                    result = (a * b).ToString();
+                    break;
+                case '/':
+                    if(b != 0)
+                        result = (a / b).ToString();
+                    break;
+            }
+            onConsoleOutput = result;
+            
+        }
+
+        private void Toggle()
+        {
+            onConsoleOutput = (- double.Parse(onConsoleOutput)).ToString();
+            if(operandSwitch == OperandSwitch.OperandA)
+                operandA = onConsoleOutput;
+            else
+                operandB = onConsoleOutput;
+        }
+
+        private void HandleDecimals()
+        {
+            if(onConsoleOutput.Contains("."))
+                return;
+
+            if(onConsoleOutput == "0")
+                onConsoleOutput = "0.";
+        }
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            switch (key)
+            {
+                case var tempKey when (tempKey >= '0' && tempKey <= '9'):
+                    if (operandSwitch == OperandSwitch.OperandA)
+                    {
+                        if (lastKey == LastKey.Digit)
+                        {
+                            if(onConsoleOutput == "0")
+                                onConsoleOutput = key.ToString();
+                            else
+                                onConsoleOutput = onConsoleOutput + key.ToString();
+                            operandA = onConsoleOutput;
+                        }
+                    }
+                    else if (operandSwitch == OperandSwitch.OperandB)
+                    {
+                        if(lastKey == LastKey.Operator)
+                            onConsoleOutput = key.ToString();
+
+                        else if (lastKey == LastKey.Digit)
+                        {
+                            if(onConsoleOutput == "0")
+                                onConsoleOutput = key.ToString();
+                            else
+                                onConsoleOutput = onConsoleOutput + key.ToString();
+                        }
+                        operandB = onConsoleOutput;
+                    }
+                    lastKeyValue = key;
+                    lastKey = LastKey.Digit;
+                    break;
+
+                case var tempKey when (tempKey == '+' || tempKey == '-' || tempKey == '/' || tempKey.ToString().ToLower() == "x"):
+                    if (operandSwitch == OperandSwitch.OperandA)
+                    {
+                        operandSwitch = OperandSwitch.OperandB;
+                        lastKey = LastKey.Operator;
+                        lastOperator = tempKey;
+                        return onConsoleOutput;
+                    }
+                    Result();
+
+                    lastOperator = tempKey;
+                    lastKey = LastKey.Operator;
+                    operandA = onConsoleOutput;
+
+                    if(operandSwitch == OperandSwitch.OperandA)
+                        operandSwitch = OperandSwitch.OperandB;
+        
+                    break;
+
+                case var tempKey when (tempKey.ToString().ToLower() == "s"):
+                    Toggle();
+                    break;
+
+                case var tempKey when (tempKey.ToString().ToLower() == "c"):
+                    Reset();
+                    break;
+
+                case var tempKey when (tempKey == '='):
+                    Result();
+                    break;
+
+                case var tempKey when (tempKey == '.'):
+                    HandleDecimals();
+                    break;
+            }
+            return onConsoleOutput;
         }
     }
 }

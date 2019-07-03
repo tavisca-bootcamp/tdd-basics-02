c7239e24f709fe974f1a65704451fac746901bde
tavisca-rpawar <rpawar@tavisca.in>
2019-07-03 10:45:52 +0530

    COnsole Calculator

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -6,9 +6,50 @@ namespace ConsoleCalculator.Tests
     public class CalculatorFixture
     {
         [Fact]
-        public void DummyTest()
+        public void TestAddition()
         {
-            return;
+            string expression = "10+2trf=";
+            string actual = GetResult(expression);
+            Assert.Equal("12", actual);
+        }
+        [Fact]
+        public void TestMultipleZero()
+        {
+            string expression = "10+000000....10.";
+            string actual = GetResult(expression);
+            Assert.Equal("0.10", actual);
+        }
+        [Fact]
+        public void TestDivideByZero()
+        {
+            string expression = "12/0.0=";
+            string actual = GetResult(expression);
+            Assert.Equal("-E-", actual);
+        }
+        [Fact]
+        public void TestClearHistory()
+        {
+            string expression = "1+2+C";
+            string actual = GetResult(expression);
+            Assert.Equal("0", actual);
+        }
+        [Fact]
+        public void TestToggleSIgn()
+        {
+            string expression = "100sss-90s=";
+            string actual = GetResult(expression);
+            Assert.Equal("-10", actual);
+        }
+
+        public string GetResult(string expression)
+        {
+            Calculator calculatorObj = new Calculator();
+            string result = "";
+            foreach(char key in expression)
+            {
+                result = calculatorObj.SendKeyPress(key);
+            }
+            return result;
         }
     }
 }
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,145 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
+        string firstOperand = "";
+        string secondOperand = "";
+        char? @operator = null;
+        List<char> validArithmeticOperator = new List<char> { '+','-','x','/'};
 
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            key = char.ToLower(key);
+            if (key == 'c')
+            {
+                ClearHistory();
+                return "0";
+            }
+            else if (key == 's')
+                return ToggleSign();
+
+            else if (char.IsDigit(key))
+                return NumbericDigitHandling(key);
+
+            else if (key == '.')
+                return DecimalHandling(key);
+
+            else if (validArithmeticOperator.Contains(key))
+            {
+                firstOperand = PerformArithmeticOperation(); //Assigning the result to firstOperand
+                if (firstOperand == "Error")
+                    return DisplayError();
+                secondOperand = "";
+                @operator = key; // Assign current operator
+                return firstOperand;
+            }
+            else if (key == '=')
+                return EqualSignPressed();
+
+            return RandomCharacterHandling();
+        } //End  of SendKeyPress function
+
+        public string ToggleSign()
+        {
+            if (@operator == null)
+            {
+                if (double.TryParse(firstOperand, out double number1))
+                    firstOperand = (-1 * number1).ToString();
+                return firstOperand;
+            }
+            if (double.TryParse(secondOperand, out double number2))
+                secondOperand = (-1 * number2).ToString();
+            return secondOperand;
+        }//End of ToggleSign Function
+
+        public string NumbericDigitHandling(char key)
+        {
+            if (@operator == null)
+            {   // Also avoiding additional 0 like 00000 -> 0
+                firstOperand = firstOperand == "0" ? key.ToString() : firstOperand + key.ToString();
+                return firstOperand;
+            }
+            secondOperand = secondOperand == "0" ? key.ToString() : secondOperand + key.ToString();
+            return secondOperand;
+        }
+        public string DecimalHandling(char key)
+        {
+            if (@operator == null) //If firstOperand
+            {
+                if (string.IsNullOrEmpty(firstOperand)) // Appending 0 like '.' -> 0.
+                    firstOperand = "0";
+
+                if (firstOperand.Contains(".") == false) //Avoiding multiple '.'
+                    firstOperand += key;
+
+                return firstOperand;
+            }
+            if (string.IsNullOrEmpty(secondOperand))
+                secondOperand = "0" + key;
+
+            else if (secondOperand.Contains(".") == false)
+                secondOperand += key;
+
+            return secondOperand;
+        } //End of DecimalHandling function
+
+        public string PerformArithmeticOperation()
+        {
+            if (double.TryParse(firstOperand, out double firstValue) == false)
+                return "Error";
+
+            if (@operator == null) //For first operation
+                return firstOperand;
+
+            if (double.TryParse(secondOperand, out double secondValue) == false)
+                return "Error";
+
+            switch (@operator)
+            {
+                case '+': return (firstValue + secondValue).ToString();
+                case '-': return (firstValue - secondValue).ToString();
+                case 'x': return (firstValue * secondValue).ToString();
+                case '/': return secondValue == 0 ? "Error" : (firstValue / secondValue).ToString();
+            }
+            return "";
+        } //End of PerformArithmeticOperation function
+
+        public string EqualSignPressed()
+        {
+            if (string.IsNullOrWhiteSpace(firstOperand))
+                return DisplayError();
+
+            if (@operator == null)
+                return firstOperand;
+
+            if (secondOperand == "") // secondOperand was not provided before hitting '='
+                secondOperand = firstOperand;
+
+            string result = SendKeyPress('+');
+            ClearHistory();
+            return result;
+        }
+
+        public string RandomCharacterHandling()
+        {
+            if (string.IsNullOrWhiteSpace(firstOperand))
+                return "";
+            if (@operator == null)
+                return firstOperand;
+            return secondOperand;
+        }
+        public string DisplayError()
+        {
+            ClearHistory();
+            return "-E-";
+        }
+        public void ClearHistory()
+        {
+            firstOperand = secondOperand = "";
+            @operator = null;
         }
     }
 }

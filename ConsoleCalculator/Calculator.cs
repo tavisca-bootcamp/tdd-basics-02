12e923af5c66fa7954aadf24051d852ddd71b5b4
tavisca-apriyadarshi <apriyadarshi@tavisca.in>
2019-07-04 00:18:14 +0530

    Q2 of A3 3/6/2019

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -4,72 +4,17 @@ namespace ConsoleCalculator
 {
     public class Calculator
     {
-        int resultInt = 0;
-        int numberInt = 0;
-        decimal resultDecimal = 0;
-        decimal numberDecimal = 0;
-        string operand = "";
-        bool isDecimal = false;
-        string checkOperator = "";
+        Double resultValue = 0;
+        String operationPerformed = "";
+        bool isOperationPerformed = false;
 
-        public string SendKeyPress(char key)
-        {
-            if(IsNumber(key) && !isDecimal){
-                operand += ""+key;
+        String displayResult = "0";
+        bool IsOperation(char input){
+            if(input=='+'||input=='-'||input=='x'||input=='/'|| input=='s'|| 
+            input=='c'||input=='='||input=='X'||input=='C'||input=='S'){
+                    return true;
             }
-            else if(IsNumber(key) && isDecimal){
-                if(operand.Equals("")){
-                    operand = "0.";
-                }
-                else{
-                    operand += key;
-                }
-            }
-            else if(IsOperation(key)){
-                if(!operand.Equals("") && !isDecimal){
-                    numberInt = Int32.Parse(operand);
-                }
-                else if(!operand.Equals("") && isDecimal){
-                    numberDecimal = Decimal.Parse(operand);
-                }
-                
-                try{
-                    switch(key){
-                        case '+': if(!isDecimal) Addition(numberInt);
-                                else Addition(numberDecimal);
-                                break;
-                        case '-': if(!isDecimal) Subtraction(numberInt);
-                                else Subtraction(numberDecimal);
-                                break;
-                        case 'x': case 'X': if(!isDecimal) Multiplication(numberInt);
-                                else Multiplication(numberDecimal);
-                                break;
-                        case '/': if(!isDecimal) Division(numberInt);
-                                else Division(numberDecimal);
-                                break;
-                        case '.': isDecimal = true;
-                                operand += ".";
-                                break;
-                        case 's': case 'S': ToggleSign();
-                                break;
-                        case 'c': case 'C': ResetAll();
-                                break;
-                        case '=': if(isDecimal){
-                                        return ""+numberDecimal;
-                                    }
-                                    return ""+numberDecimal;
-                        //default : break;
-                        
-                    }
-                }catch{
-                    return "-E-";
-                }
-                
-            }
-            if(isDecimal){
-                    return ""+numberDecimal;
-                }
-                return ""+numberDecimal;
+            return false;
         }
 
         bool IsNumber(char input){
@@ -79,67 +24,91 @@ namespace ConsoleCalculator
                     return true;
                 }
             }catch{
-                
+                return false;
             }
             
             return false;
         }
 
-        bool IsOperation(char input){
-            if(input=='+'||input=='-'||input=='x'||input=='/'||input=='.'||
-                input=='s'|| input=='c'||input=='='||input=='X'||input=='C'||input=='S'){
-                    return true;
+        public string SendKeyPress(char key)
+        {
+            isOperationPerformed = false;
+
+            if(IsOperation(key)){
+                operator_click(key);
             }
-            return false;
-        }
 
-        /*bool NotRepeatingOperation(char input){
-            if(input=='+'||input=='-'||input=='x'||input=='/'||input=='.'||
-                input=='s'|| input=='c'||input=='='||input=='X'||input=='C'||input=='S'){
-                    return true;
+            if (key == '.')
+            { 
+               if(!displayResult.Contains("."))
+                   displayResult = displayResult + key;
+
+            }else if(IsNumber(key)){
+                if ((displayResult == "0") || (isOperationPerformed)){
+                    if(key != '0')
+                        displayResult = ""+key;
+                }
+                else
+                    displayResult = displayResult + key;
             }
-        }*/
-        void Addition(int  operand){
-            resultInt += operand;
-        }
-        void Subtraction(int operand){
-            resultInt -= operand;
-        }
-        void Multiplication(int operand){
-            resultInt *= operand;
-        }
-        void Division(int operand){
-            resultInt /= operand;
-        }
-        
-        void Addition(decimal operand){
-            resultDecimal += operand;
-        }
-        void Subtraction(decimal operand){
-            resultDecimal -= operand;
-        }
-        void Multiplication(decimal operand){
-            resultDecimal *= operand;
-        }
-        void Division(decimal operand){
-            resultDecimal /= operand;
+
+            
+            return displayResult;
         }
 
-        void ToggleSign(){
-            resultInt *= -1;
-            resultDecimal *= -1;
-        }
-        void ResetAll(){
-            resultInt = 0;
-            resultDecimal = 0;
-            numberInt = 0;
-            numberDecimal = 0;
-            operand = "";
-            isDecimal = false;
-            checkOperator = "";
-        }
-        /* void ReturnResult(){
+        private void operator_click(char key)
+        {
 
-        }*/
+            if (resultValue != 0)
+            {
+                PerformCalculation();
+                operationPerformed = ""+key;
+                isOperationPerformed = true;
+            }
+            else
+            {
+
+                operationPerformed = ""+key;
+                resultValue = Double.Parse(displayResult);
+                isOperationPerformed = true;
+            }
+        }
+
+        private void ToggleSign()
+        {
+            resultValue *= -1;
+        }
+
+        private void ResetAll()
+        {
+            displayResult = "0";
+            resultValue = 0;
+        }
+
+        private void PerformCalculation()
+        {
+            switch (operationPerformed)
+            {
+                case "+":
+                    displayResult = (resultValue + Double.Parse(displayResult)).ToString();
+                    break;
+                case "-":
+                    displayResult = (resultValue - Double.Parse(displayResult)).ToString();
+                    break;
+                case "x": case "X":
+                    displayResult = (resultValue * Double.Parse(displayResult)).ToString();
+                    break;
+                case "/":
+                    displayResult = (resultValue / Double.Parse(displayResult)).ToString();
+                    break;
+                case "s": case "S": ToggleSign();
+                    break;
+                case "c": case "C": ResetAll();
+                    break;
+                default:
+                    break;
+            }
+            resultValue = Double.Parse(displayResult);
+        }
     }
 }

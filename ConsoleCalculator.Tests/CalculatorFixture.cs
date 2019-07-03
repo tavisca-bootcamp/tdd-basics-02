ff0e38d18166ef185fedff9bffd36aca9b7a91ff
asharma@tavisca.in <asharma@tavisca.in>
2019-07-04 00:15:27 +0530

    Calculator Console

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -5,10 +5,136 @@ namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+        Calculator calculator = new Calculator();
+
         [Fact]
         public void DummyTest()
         {
             return;
         }
+
+        [Fact]
+        public void TestOperand()
+        {
+            calculator.SendKeyPress('1');
+            Assert.Equal("15", calculator.SendKeyPress('5'));
+            return;
+
+        }
+
+        [Fact]
+        public void TestOperator()
+        {
+            calculator.SendKeyPress('1');
+            calculator.SendKeyPress('+');
+            calculator.SendKeyPress('5');
+            Assert.Equal("6", calculator.SendKeyPress('='));
+            Assert.Equal("0", calculator.SendKeyPress('c'));
+        }
+
+
+        [Fact]
+        public void TestOperationAdd()
+        {
+            calculator.SendKeyPress('1');
+            calculator.SendKeyPress('0');
+            calculator.SendKeyPress('+');
+            calculator.SendKeyPress('5');
+            Assert.Equal("15", calculator.SendKeyPress('='));
+            calculator.SendKeyPress('c');
+        }
+
+        [Fact]
+        public void TestOperationSubtract()
+        {
+            calculator.SendKeyPress('1');
+            calculator.SendKeyPress('0');
+            calculator.SendKeyPress('-');
+            calculator.SendKeyPress('5');
+            Assert.Equal("5", calculator.SendKeyPress('='));
+            calculator.SendKeyPress('c');
+
+        }
+
+        [Fact]
+        public void TestOperationMultiply()
+        {
+            calculator.SendKeyPress('1');
+            calculator.SendKeyPress('0');
+            calculator.SendKeyPress('x');
+            calculator.SendKeyPress('5');
+            Assert.Equal("50", calculator.SendKeyPress('='));
+            calculator.SendKeyPress('c');
+        }
+
+        [Fact]
+        public void TestOperationDivide()
+        {
+            calculator.SendKeyPress('1');
+            calculator.SendKeyPress('0');
+            calculator.SendKeyPress('/');
+            calculator.SendKeyPress('5');
+            Assert.Equal("2", calculator.SendKeyPress('='));
+            calculator.SendKeyPress('c');
+        }
+
+        [Fact]
+        public void TestOperationDivideByZero()
+        {
+            calculator.SendKeyPress('1');
+            calculator.SendKeyPress('0');
+            calculator.SendKeyPress('/');
+            calculator.SendKeyPress('0');
+            Assert.Equal("-E-", calculator.SendKeyPress('='));
+            calculator.SendKeyPress('c');
+        }
+
+        [Fact]
+        public void TestInvalidKey()
+        {
+            Assert.Equal("-E-", calculator.SendKeyPress('o'));
+            calculator.SendKeyPress('c');
+            Assert.Equal("0", calculator.SendKeyPress('0'));
+            calculator.SendKeyPress('c');
+        }
+
+
+        [Fact]
+        public void TestDecimalFirstOperand()
+        {
+            calculator.SendKeyPress('1');
+            calculator.SendKeyPress('0');
+            calculator.SendKeyPress('.');
+            calculator.SendKeyPress('5');
+            Assert.Equal("10.50", calculator.SendKeyPress('0'));
+            calculator.SendKeyPress('c');
+        }
+
+        [Fact]
+        public void TestDecimalSecondOperand()
+        {
+            calculator.SendKeyPress('1');
+            calculator.SendKeyPress('+');
+            calculator.SendKeyPress('5');
+            calculator.SendKeyPress('.');
+            calculator.SendKeyPress('8');
+            Assert.Equal("6.8", calculator.SendKeyPress('='));
+            calculator.SendKeyPress('c');
+        }
+
+        [Fact]
+        public void TestToggleSign()
+        {
+            calculator.SendKeyPress('1');
+            calculator.SendKeyPress('0');
+            calculator.SendKeyPress('+');
+            calculator.SendKeyPress('5');
+            calculator.SendKeyPress('s');
+            Assert.Equal("5", calculator.SendKeyPress('='));
+            calculator.SendKeyPress('c');
+        }
     }
 }
+
+
+
--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,232 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
+        public string firstOperand;
+        public string secondOperand;
+        public string result;
+        public char lastOperator;
+        public bool validOperand;
+        public bool validOperator;
+
+
+        public Calculator()
+        {
+            firstOperand = null;
+            secondOperand = null;
+            result = "";
+            lastOperator = 'N';
+            validOperand = false;
+            validOperator = false;
+
+        }
 
         public string SendKeyPress(char key)
         {
             // Add your implementation here.
-            throw new NotImplementedException();
+
+            validOperand = false;
+
+            // to check if key is valid or not
+            switch (key)
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '.':
+                    validOperand = IsValidOperand(key);
+                    break;
+
+                case '+':
+                case '-':
+                case 'x':
+                case 'X':
+                case '/':
+                    validOperator = IsValidOperator(key);
+                    lastOperator = key;
+                    break;
+
+                case '=':
+                case 'c':
+                case 'C':
+                    validOperator = IsValidOperator(key);
+                    break;
+                case 's':
+                case 'S':
+                    validOperator = IsValidOperator(key);
+                    break;
+            }
+            // obtaining first operand
+            if (validOperator == false && validOperand == true)
+            {
+                firstOperand += key;
+                result = firstOperand;
+            }
+
+            // obtaining second operand
+            if (validOperator == true && validOperand == true)
+            {
+                secondOperand += key;
+                result = secondOperand;
+            }
+
+            if (validOperand == false && validOperator == false)
+                result = "-E-";
+
+            //obtaining decimal numbers
+            if (key == '.')
+            {
+                if (validOperator == false)
+                {
+                    firstOperand = DecimalPoint(firstOperand);
+                    result = firstOperand;
+                }
+
+                else
+                {
+                    secondOperand = DecimalPoint(secondOperand);
+                    result = secondOperand;
+                }
+
+            }
+
+            if (key == 'c' || key == 'C')
+                result = ClearConsole();
+
+            if (key == 's' || key == 'S')
+            {
+
+                // change sign of first operand as operator is not pressed yet nor second operand
+                if (secondOperand.Equals(null))
+                {
+                    firstOperand = ToggleSign(firstOperand);
+                    result = firstOperand;
+                }
+
+                // change sign of second operand
+                else
+                {
+                    secondOperand = ToggleSign(secondOperand);
+                    result = secondOperand;
+                }
+
+            }
+            if (!string.Equals(firstOperand, null) && !string.Equals(secondOperand, null) && key == '=')
+            {
+
+                if (lastOperator == '+') // Addition
+                    result = Add(firstOperand, secondOperand);
+                if (lastOperator == '-')  // Subtraction
+                    result = Subtract(firstOperand, secondOperand);
+                if (lastOperator == 'x' || lastOperator == 'X')  // Multiplication
+                    result = Multiply(firstOperand, secondOperand);
+                if (lastOperator == '/')  //Division
+                    result = Divide(firstOperand, secondOperand);
+
+            }
+
+            return result;
+
+            // throw new NotImplementedException();
+        }
+
+        public bool IsValidOperator(char key)
+        {
+            char[] validOperator = { '+', '-', '/', '=', 'x', 'X', 'c', 'C', 's', 'S' };
+            //if(validOperator.Contains(key) == true)
+            return validOperator.Contains(key);
+        }
+
+        public bool IsValidOperand(char key)
+        {
+            char[] validOperand = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            return validOperand.Contains(key);
+        }
+
+        public string DecimalPoint(string operand)
+        {
+            // adding decimal point when no numeric value is present and . key is pressed
+            if (string.Equals(operand, null))
+                operand = "0.";
+            else
+            {
+                // adding decimal point when numeric value is present
+                if (!operand.Contains('.') && Convert.ToInt32(operand) != 0)
+                    operand += ".";
+                // ignoring multiple decimal points and ignoring multiple zeroes before decimal point
+                if (operand.Contains('.'))
+                {
+                    if (Convert.ToDouble(operand) == 0.0)
+                        operand = "0.";
+                }
+
+            }
+            return operand;
+        }
+
+        public string Add(string operand1, string operand2)
+        {
+            // addition of two numerics            
+            double sum = Convert.ToDouble(operand1) + Convert.ToDouble(operand2);
+            return sum.ToString();
+        }
+
+        public string Subtract(string operand1, string operand2)
+        {
+            // subtraction of two numerics
+            double difference = Convert.ToDouble(operand1) - Convert.ToDouble(operand2);
+            return difference.ToString();
+        }
+
+        public string Multiply(string operand1, string operand2)
+        {
+            // multiplication of two numerics
+            double product = Convert.ToDouble(operand1) * Convert.ToDouble(operand2);
+            return product.ToString();
+        }
+
+        public string Divide(string operand1, string operand2)
+        {
+            // division of two numerics
+            double quotient;
+            try
+            {
+                if (Convert.ToDouble(operand2) == 0.0)
+                    throw new DivideByZeroException();
+                else
+                    quotient = Convert.ToDouble(operand1) / Convert.ToDouble(operand2);
+            }
+            catch (DivideByZeroException)
+            {
+                return "-E-";
+            }
+            return quotient.ToString();
+        }
+
+        // to toggle sign
+        public string ToggleSign(string operand1)
+        {
+            return (-1 * (Convert.ToDouble(operand1))).ToString();
+        }
+
+        // to clear console
+        public string ClearConsole()
+        {
+            return "0";
         }
     }
 }
+
+
+
+

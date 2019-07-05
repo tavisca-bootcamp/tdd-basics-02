a1bf133618568aeb13651a2905a075592e114003
tavisca-rdimri <rdimri@tavisca.in>
2019-07-05 11:22:20 +0530

    tdd basic 2 code

---
+++ ConsoleCalculator/Operations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    class Operations
+    {
+        public static string Addition(double operandOne, double operandTwo)
+        {
+            double temp = operandOne + operandTwo;
+
+            if (temp % 1 != 0.0)
+                return temp.ToString();
+            int result = (int)temp;
+            return result.ToString();
+
+        }
+        public static string Subtraction(double operandOne, double operandTwo)
+        {
+            double temp = operandOne - operandTwo;
+
+            if (temp % 1 != 0.0)
+                return temp.ToString();
+            int result = (int)temp;
+            return result.ToString();
+
+        }
+        public static string Multiplication(double operandOne, double operandTwo)
+        {
+            double temp = operandOne * operandTwo;
+
+            if (temp % 1 != 0.0)
+                return temp.ToString();
+            int result = (int)temp;
+            return result.ToString();
+
+        }
+        public static string Division(double operandOne, double operandTwo)
+        {
+            if ((int)operandTwo == 0)
+                return "-E-";
+            double temp = operandOne / operandTwo;
+
+            if (temp % 1 != 0.0)
+                return temp.ToString();
+            int result = (int)temp;
+            return result.ToString();
+
+        }
+
+    }
+}

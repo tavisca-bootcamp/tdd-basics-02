f0471b3184fc84d6d4d23c712485c64c181b1f95
tavisca-rrai <rrai@tavisca.in>
2019-07-05 17:27:57 +0530

    calculator console

---
+++ ConsoleCalculator/Operation.cs
@@ -0,0 +1,22 @@
+namespace ConsoleCalculator
+{
+    public class Operation
+    {
+        public static  double Add(double operand1, double operand2)
+        {
+            return operand1 + operand2;
+        }
+        public static double Subtract(double operand1, double operand2)
+        {
+            return operand1 - operand2;
+        }
+        public static double Multiply(double operand1, double operand2)
+        {
+            return operand1 * operand2;
+        }
+        public static double Divide(double operand1, double operand2)
+        {
+            return operand1 / operand2;
+        }
+    }
+}

53a64bd23d2d07a6d75027f8ef45c458536ea5da
raggarwal <raggarwal@tavisca.in>
2019-07-08 23:44:40 +0530

    updated program

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -14,28 +14,22 @@ namespace ConsoleCalculator
         {
             if (isValidCharacters(key))
             {
-                if (key >= '0' && key <= '9')              //for digits
+                if (isDigit(key))              //for digits
                 {
                     if (countOperators == 0)             //for first number
                     {
-                        if (!(firstNumber == "0" && key == '0'))
-                        {
-                            firstNumber = firstNumber + key;
-                        }
+                        firstNumber = returnFirstNum(key, firstNumber);
                     }
                     else                           //for second number
                     {
-                        if (!(secondNumber == "0" && key == '0'))
-                        {
-                            secondNumber = secondNumber + key;
-                        }
+                        secondNumber = returnSecondNum(key, secondNumber);
                         return secondNumber;
                     }
                 }
                 else if (isValidOperator(key))
                 {
                     countOperators = countOperators + 1;
-                    if (countOperators == 2)
+                    if (countOperators == 2)                //if stored operator is more than 1 than evaluates last operator first
                     {
                         calculateOperation();
                         secondNumber = "";
@@ -43,7 +37,7 @@ namespace ConsoleCalculator
                     }
                     operation = key;
                 }
-                else if (key == '=')
+                else if (key == '=')                 //if operator is '=' than it calculate the equation
                 {
                     calculateOperation();
                     countOperators = countOperators - 1;
@@ -52,30 +46,16 @@ namespace ConsoleCalculator
                 {
                     if (countOperators == 0 && flagForFirst == 0)                    //for first number
                     {
-                        if (firstNumber.Length == 0)
-                        {
-                            firstNumber = firstNumber + "0.";
-                        }
-                        else
-                        {
-                            firstNumber = firstNumber + ".";
-                        }
+                        firstNumber = returnDotFirstNum(firstNumber);
                         flagForFirst = 1;
                     }
                     else if (countOperators == 1 && flagForSecond == 0)               //for second number
                     {
-                        if (secondNumber.Length == 0)
-                        {
-                            secondNumber = secondNumber + "0.";
-                        }
-                        else
-                        {
-                            secondNumber = secondNumber + ".";
-                        }
+                        secondNumber = returnDotSecondNum(secondNumber);
                         flagForSecond = 1;
                     }
                 }
-                else if (key == 'c' || key == 'C')
+                else if (key == 'c' || key == 'C')                  // for clear
                 {
                     firstNumber = "0";
                     secondNumber = "0";
@@ -84,7 +64,7 @@ namespace ConsoleCalculator
                     flagForFirst = 0;
                     flagForSecond = 0;
                 }
-                else if (key == 's' || key == 'S')
+                else if (key == 's' || key == 'S')               //for toggle
                 {
                     if (countOperators == 1)
                     {
@@ -101,8 +81,61 @@ namespace ConsoleCalculator
             {
                 return firstNumber;
             }
-            throw new NotImplementedException();
         }
+
+        public static string returnFirstNum(char key, string firstNumber)    //for return first number
+        {
+            if (!(firstNumber == "0" && key == '0'))
+            {
+                firstNumber = firstNumber + key;
+            }
+            return firstNumber;
+        }
+
+        public static string returnSecondNum(char key, string secondNumber)    //for return second number
+        {
+            if (!(secondNumber == "0" && key == '0'))
+            {
+                secondNumber = secondNumber + key;
+            }
+            return secondNumber;
+        }
+
+        public Boolean isDigit(char key)        //to check whether it is digit or not
+        {
+            if (key >= '0' && key <= '9')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string returnDotFirstNum(string firstNumber)
+        {
+            if (firstNumber.Length == 0)
+            {
+                firstNumber = firstNumber + "0.";
+            }
+            else
+            {
+                firstNumber = firstNumber + ".";
+            }
+            return firstNumber;
+        }
+
+        public string returnDotSecondNum(string secondNumber)
+        {
+            if (secondNumber.Length == 0)
+            {
+                secondNumber = secondNumber + "0.";
+            }
+            else
+            {
+                secondNumber = secondNumber + ".";
+            }
+            return secondNumber;
+        }
+
         public void calculateOperation()
         {
             if(operation == '+')
@@ -129,6 +162,7 @@ namespace ConsoleCalculator
                 firstNumber = (double.Parse(firstNumber) * double.Parse(secondNumber)).ToString();
             }
         }
+
         public bool isValidOperator(char key)
         {
             if(key == '+' || key == '-' || key == '/' || key == '*')
@@ -137,6 +171,7 @@ namespace ConsoleCalculator
             }
             return false;
         }
+
         public bool isValidCharacters(char key)
         {
             if(char.IsDigit(key) || isValidOperator(key) || key == '=' || key =='.' || key == 's' ||key == 'S' || key == 'c' || key == 'C')

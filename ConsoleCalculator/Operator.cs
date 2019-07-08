f972680f813b5cd566fe43daedecf8fc5b0e6107
Dipika Mandal <dmandal@tavisca.in>
2019-07-08 08:02:49 +0530

    Implemented an Operator class for holding the current operator to work with

---
+++ ConsoleCalculator/Operator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Operator
+{
+    public char Symbol;
+    public Operator()
+    {
+        Symbol = ' ';
+    }
+    public void SetOperator(char newOperator)
+    {
+        Symbol = newOperator;
+    }
+    public char GetOperator()
+    {
+        return Symbol;
+    }
+}

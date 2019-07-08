70a11716897e0d1f2b166f54e62ddac3b89a4e5f
Dipika Mandal <dmandal@tavisca.in>
2019-07-08 08:04:40 +0530

    Implemented ResultTillNow class to hold the result calculated till now.

---
+++ ConsoleCalculator/ResultTillNow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class ResultTillNow
+{
+    double Value;
+    string Result;
+	public ResultTillNow()
+	{
+        Value = 0;
+        Result="0";
+	}
+    public void SetValue(string newValue)
+    {
+        Value = Double.Parse(newValue);
+        Result=newValue;
+    }
+    public string GetValue()
+    {
+        return Result;
+    }
+    public void Operate(char operation,string operand)
+    {
+        //todo
+    }
+}

f2ba19997bbeb5ff97a9b97fad47056adbfdeaa1
Dipika Mandal <dmandal@tavisca.in>
2019-07-08 08:38:39 +0530

    Implemented SendKeyPress method and added few helping methods for handling different functionalities of the calculator

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -17,8 +17,63 @@ namespace ConsoleCalculator
         }
         public string SendKeyPress(char key)
         {
+            switch(key){
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    NumberPressed(key);
+                break;
+                case '+':
+                case '-':
+                case 'x':
+                case 'X':
+                case '/':
+                    OperatorPressed(key);
+                break;
+                case 's':
+                case 'S':
+                    Toggle();
+                break;
+                case 'c':
+                case 'C':
+                    Reset();
+                break;
+                case '.':
+                    DecimalAdded();
+                break;
+                case '=':
+                    EqualPressed();
+                break;
+                default:
+                    //do nothing
+                break;
+            }
+            return CalculatorDisplay.ShowOutput();
+        }
+        public void NumberPressed(char number){
             //todo
-            throw new NotImplementedException();
+        }
+        public void OperatorPressed(char number){
+            //todo
+        }
+        public void Toggle(){
+            //todo
+        }
+        public void Reset(){
+            //todo
+        }
+        public void DecimalAdded(){
+            //todo
+        }
+        public void EqualPressed(){
+            //todo
         }
     }
 }

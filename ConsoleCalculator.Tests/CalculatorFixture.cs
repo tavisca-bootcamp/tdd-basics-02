0b09a25595909c841c93eae01213d2dda89744a9
Rishabh <rjaiswal@tavisca.in>
2019-07-08 17:09:11 +0530

    Test Cases structured to AAA Pattern

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ConsoleCalculator.Tests
@@ -10,68 +11,133 @@ namespace ConsoleCalculator.Tests
         [Fact]
         public void Test1()
         {
-            Assert.Equal("1", c.SendKeyPress('1'));
-            Assert.Equal("10", c.SendKeyPress('0'));
-            Assert.Equal("10", c.SendKeyPress('+'));
-            Assert.Equal("2", c.SendKeyPress('2'));
-            Assert.Equal("12", c.SendKeyPress('='));
+            //Arrange
+            List<char> sendKey = new List<char> { '1', '0', '+', '2', '=' };
+            List<string> expected = new List<string> {"1", "10", "10", "2", "12" };
+            List<string> actual = new List<string>();
+
+            //Act
+            foreach(char key in sendKey)
+            {
+                actual.Add(c.SendKeyPress(key));
+            }
+
+            //Assert
+            for(int i=0;i<expected.Count;i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+
         }
 
         [Fact]
         public void Test2()
         {
-            Assert.Equal("1", c.SendKeyPress('1'));
-            Assert.Equal("10", c.SendKeyPress('0'));
-            Assert.Equal("10", c.SendKeyPress('/'));
-            Assert.Equal("0", c.SendKeyPress('0'));
-            Assert.Equal("-E-", c.SendKeyPress('='));
+            //Arrange
+            List<char> sendKey = new List<char> { '1', '0', '/', '0', '=' };
+            List<string> expected = new List<string> { "1", "10", "10", "0", "-E-" };
+            List<string> actual = new List<string>();
+
+            //Act
+            foreach (char key in sendKey)
+            {
+                actual.Add(c.SendKeyPress(key));
+            }
+
+            //Assert
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+
         }
 
         [Fact]
         public void Test3()
         {
-            Assert.Equal("0", c.SendKeyPress('0'));
-            Assert.Equal("0", c.SendKeyPress('0'));
-            Assert.Equal("0.", c.SendKeyPress('.'));
-            Assert.Equal("0.", c.SendKeyPress('.'));
-            Assert.Equal("0.0", c.SendKeyPress('0'));
-            Assert.Equal("0.00", c.SendKeyPress('0'));
-            Assert.Equal("0.001", c.SendKeyPress('1'));
+            //Arrange
+            List<char> sendKey = new List<char> { '0', '0', '.', '.', '0', '0', '1' };
+            List<string> expected = new List<string> { "0", "0", "0.", "0.", "0.0", "0.00", "0.001" };
+            List<string> actual = new List<string>();
+
+            //Act
+            foreach (char key in sendKey)
+            {
+                actual.Add(c.SendKeyPress(key));
+            }
+
+            //Assert
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+
         }
 
         [Fact]
         public void Test4()
         {
-            Assert.Equal("1", c.SendKeyPress('1'));
-            Assert.Equal("12", c.SendKeyPress('2'));
-            Assert.Equal("12", c.SendKeyPress('+'));
-            Assert.Equal("2", c.SendKeyPress('2'));
-            Assert.Equal("-2", c.SendKeyPress('S'));
-            Assert.Equal("2", c.SendKeyPress('S'));
-            Assert.Equal("-2", c.SendKeyPress('S'));
-            Assert.Equal("10", c.SendKeyPress('='));
+            //Arrange
+            List<char> sendKey = new List<char> { '1', '2', '+', '2', 'S', 'S', 'S', '=' };
+            List<string> expected = new List<string> { "1", "12", "12", "2", "-2", "2", "-2", "10" };
+            List<string> actual = new List<string>();
+
+            //Act
+            foreach (char key in sendKey)
+            {
+                actual.Add(c.SendKeyPress(key));
+            }
+
+            //Assert
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+
         }
 
         [Fact]
         public void Test5()
         {
-            Assert.Equal("1", c.SendKeyPress('1'));
-            Assert.Equal("1", c.SendKeyPress('+'));
-            Assert.Equal("2", c.SendKeyPress('2'));
-            Assert.Equal("3", c.SendKeyPress('+'));
-            Assert.Equal("3", c.SendKeyPress('3'));
-            Assert.Equal("6", c.SendKeyPress('+'));
-            Assert.Equal("12", c.SendKeyPress('='));
+            //Arrange
+            List<char> sendKey = new List<char> { '1', '+', '2', '+', '3', '+', '=' };
+            List<string> expected = new List<string> { "1", "1", "2", "3", "3", "6", "12" };
+            List<string> actual = new List<string>();
+
+            //Act
+            foreach (char key in sendKey)
+            {
+                actual.Add(c.SendKeyPress(key));
+            }
+
+            //Assert
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+
         }
 
         [Fact]
         public void Test6()
         {
-            Assert.Equal("1", c.SendKeyPress('1'));
-            Assert.Equal("1", c.SendKeyPress('+'));
-            Assert.Equal("2", c.SendKeyPress('2'));
-            Assert.Equal("3", c.SendKeyPress('+'));
-            Assert.Equal("0", c.SendKeyPress('c'));
+            //Arrange
+            List<char> sendKey = new List<char> { '1', '+', '2', '+', 'c' };
+            List<string> expected = new List<string> { "1", "1", "2", "3", "0" };
+            List<string> actual = new List<string>();
+
+            //Act
+            foreach (char key in sendKey)
+            {
+                actual.Add(c.SendKeyPress(key));
+            }
+
+            //Assert
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+
         }
     }
 }

718f5cabd146b47e974b97c105fa0595ee3d3bd9
Singh <csingh@internal.tavisca.com>
2019-08-02 06:43:11 +0530

    made necessary changes

--- ConsoleCalculator.Tests/CalculatorFixture.cs
+++ ConsoleCalculator.Tests/CalculatorFixture.cs
@@ -1,14 +1,87 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ConsoleCalculator.Tests
 {
     public class CalculatorFixture
     {
+        
+        Calculator _Calculator = new Calculator();
         [Fact]
-        public void DummyTest()
+        public void When_User_Press_Invalid_Key()
         {
-            return;
+            //given
+            var actual = _Calculator.SendKeyPress('e');
+
+            //when
+            var expected = "-E-";
+
+            //then
+            Assert.Equal(expected, actual);  
+        }
+
+        [Fact]
+        public void When_UserTypedNumeric_FirstTime()
+        {
+            //when
+            var actual = _Calculator.SendKeyPress('2');
+            //given
+            var expected = "2";
+            //then
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void When_UserTypedNumeric_Repeatedly()
+        {
+            var actual = _Calculator.SendKeyPress('2');
+            actual = _Calculator.SendKeyPress('3');
+
+            var expected = "23";
+
+            Assert.Equal(expected, actual);
+
+        }
+        [Fact]
+        public void When_UserPress_ArithematicOperators()
+        {
+            var actual = _Calculator.SendKeyPress('+');
+
+            var expected = "+";
+
+            Assert.Equal(expected, actual);
+
+        }
+        [Fact]
+        public void When_UserpressNumeric_AfterOperator()
+        {
+            var actual = _Calculator.SendKeyPress('3');
+
+            var expected = "3";
+
+            Assert.Equal(expected, actual);
+
+        }
+    
+        [Fact]
+
+        public void When_User_PressC()
+        {
+            //given
+            var actual = _Calculator.SendKeyPress('C');
+            //when
+            var expected = "0";
+            //then
+            Assert.Equal("0", actual);
+
+        }
+       [Fact]
+       public void when_UserPress_Equal()
+        {
+            var actual = _Calculator.SendKeyPress('=');
+            var expected = "=";
+            //user not pressed first and secind number
+            Assert.Equal(expected, actual);
         }
     }
 }

ba0557f97719ffeadb800b0eb83e4aff7f0a6220
MadhuraAgharkar <70260114+MadhuraAgharkar@users.noreply.github.com>
2020-09-02 20:36:11 +0530

    Implementation with All passed test cases

---
+++ ConsoleCalculator/KeyType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    public enum KeyType
+    {
+        Default = 0,
+        Number,
+        DecimalPoint,
+        MathOperation,
+        EqualTo,
+        Toggle,
+        Reset,
+        NotAllowedCharacter
+    }
+}
---
+++ ConsoleCalculator/MathCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCalculator
+{
+    public static class MathCalculator
+    {
+        private static string result = null;
+        private const string ErrorCode = "-E-";
+
+        public static string Add(float firstNumber, float secondNumber)
+        {
+            result = (firstNumber + secondNumber).ToString();
+            return result;
+
+        }
+        public static string Subtract(float firstNumber, float secondNumber)
+        {
+            result = (firstNumber - secondNumber).ToString();
+            return result;
+        }
+        public static string Multiply(float firstNumber, float secondNumber)
+        {
+            result = (firstNumber * secondNumber).ToString();
+            return result;
+        }
+        public static string Divide(float firstNumber, float secondNumber)
+        {
+            if (secondNumber == 0)
+            {
+                result = ErrorCode;
+            }
+            else
+            {
+                result = (firstNumber / secondNumber).ToString();
+            }
+            return result;
+        }
+    }
+}

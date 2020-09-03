2882d3a725328838fc1ef7a3ace13d917aff6bef
bhushankale-tavisca <63724176+bhushanrkale@users.noreply.github.com>
2020-09-03 17:03:28 +0530

    TDD2-Console Calculator-Initial test suite and models

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -1,14 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ConsoleCalculator
 {
     public class Calculator
     {
-
         public string SendKeyPress(char key)
         {
-            // Add your implementation here.
-            throw new NotImplementedException();
+            return string.Empty;
+        }
+
+        private void CalculateResult()
+        {
+        }
+
+        private void HandleOperatorInput(char key)
+        {
+        }
+
+        private void ChangeSign()
+        {
+        }
+
+        private void HandleDecimalPointInput(char key)
+        {
+        }
+
+        private void HandleNumberInput(char key)
+        {
         }
     }
 }
